4500e1ac1f3cf32a742dbd090766babc26b81669
Matt Norman <normanmr@ornl.gov>
2017-05-04 09:06:44 -0400

    Making my way toward a standalone build
    CMake did a horrible job of resolving dependencies after wasting a day trying to make it do so. So, since CAM's build process works, I'm mimicking it for the standalone build. So far, I've mostly replicated CAM's configure script with a much easier to understand setup.sh script, and the mkDepends is working well so far. Now, it seems to mainly be a process of getting in all the right directories and CPP definitions.
    
    I'm including all source files from all directories needed eventually by crm_module.F90, though I'm likely not compiling them all, since it's doubtful every source file in these directories feeds into crm_module. For the make target, I'm targetting crm_module.F90 in order to create a crm.a library, which I'll then use for a further crm_standalone target.
    
    [BFB] - Bit-For-Bit

---
+++ components/cam/src/physics/crm/standalone/utils/setup.sh
@@ -0,0 +1,184 @@
+#!/bin/bash
+
+function usage {
+  echo "./setup.sh -crm_root <path> [ ... ]"
+  echo "REQUIRED:"
+  echo "   -crm_root <path>                Absolute path to the crm source root (/[...]/crm)"
+  echo "   -build_root <path>              Absolute path to the build directory root"
+  echo "OPTIONAL:"
+  echo "   -nlev <n>                       Number of CAM vertical levels"
+  echo "   -crm_nx <n>                     CRM's x-grid."
+  echo "   -crm_ny <n>                     CRM's y-grid."
+  echo "   -crm_nz <n>                     CRM's z-grid."
+  echo "   -crm_dx <n>                     CRM's horizontal grid spacing."
+  echo "   -crm_dt <n>                     CRM's timestep."
+  echo "   -SPCAM_microp_scheme <string>   CRM microphysics package name [sam1mom | m2005 ]."
+  echo "   -clubb_crm                      CRM with clubb treatment"
+  echo "   -use_ECPP                       Use CRM clouds for vertical transport, aqueous chemistry and wet removable of aerosols "
+  echo "   -use_crm_cldfrac                Use fractional cloudiness in CRM"
+  echo "   -crm_adv                        CRM advection scheme [MPDATA | UM5]"
+  echo "   -h | --help                     Print help"
+  exit 0
+}
+
+#$cfg_cppdefs .= " -DPLEV=$nlev -DPCNST=$nadv -DPCOLS=$pcols -DPSUBCOLS=$psubcols";
+
+
+##########################################################################################
+## READ IN COMMAND LINE PARAMETERS
+##########################################################################################
+
+#Defaults
+BUILD_ROOT=""
+CRM_ROOT=""
+PLEV=30
+CRM_NX=32
+CRM_NY=1
+CRM_NZ=28
+CRM_DX=1000
+CRM_DT=5
+MICRO=m2005
+CLUBB_CRM=0
+USE_ECPP=0
+USE_CRM_CLDFRAC=0
+ADV=MPDATA
+yes3Dval=0
+
+while [[ $# -gt 0 ]]
+do
+key="$1"
+
+case $key in
+    -crm_root)
+    CRM_ROOT="$2"
+    shift # past argument
+    ;;
+    -build_root)
+    BUILD_ROOT="$2"
+    shift # past argument
+    ;;
+    -nlev)
+    PLEV="$2"
+    shift # past argument
+    ;;
+    -crm_nx)
+    CRM_NX="$2"
+    shift # past argument
+    ;;
+    -crm_ny)
+    CRM_NX="$2"
+    shift # past argument
+    ;;
+    -crm_nz)
+    CRM_NZ="$2"
+    shift # past argument
+    ;;
+    -crm_dx)
+    CRM_DX="$2"
+    shift # past argument
+    ;;
+    -crm_dt)
+    CRM_DT="$2"
+    shift # past argument
+    ;;
+    -SPCAM_microp_scheme)
+    MICRO="$2"
+    shift # past argument
+    ;;
+    -crm_adv)
+    ADV=$2
+    shift # past argument
+    ;;
+    -clubb_crm)
+    CLUBB_CRM=1
+    ;;
+    -use_ECPP)
+    USE_ECPP=1
+    ;;
+    -use_crm_cldfrac)
+    USE_CRM_CLDFRAC=1
+    ;;
+    -h|--help)
+    usage
+    ;;
+    *)
+            # unknown option
+    ;;
+esac
+shift # past argument or value
+done
+
+##########################################################################################
+## CHECK SOME OF THE PARAMETERS
+##########################################################################################
+[ "$CRM_ROOT"   == "" ] && echo "Must specify -crm_root <path> as a parameter" && exit -1
+[ "$BUILD_ROOT" == "" ] && echo "Must specify -build_root <path> as a parameter" && exit -1
+[ "$MICRO" != "sam1mom" ] && [ "$MICRO" != "m2005" ] && echo "-SPCAM_microp_scheme must be [m2005 | sam1mom]" && exit -1
+[ "$ADV"   != "MPDATA"  ] && [ "$ADV"   != "UM5"   ] && echo "-crm_adv must be [MPDATA | UM5]" && exit -1
+[ "$CRM_NY" -gt "1" ] && yes3Dval=1
+
+mkdir -p $BUILD_ROOT
+SCRIPTS=`pwd`
+cd $BUILD_ROOT
+
+##########################################################################################
+## Create the preprocessor define flags
+##########################################################################################
+CPPDEFS=" -DCRM -D$MICRO -DYES3DVAL=$yes3Dval -DCRM_NX=$CRM_NX -DCRM_NY=$CRM_NY -DCRM_NZ=$CRM_NZ -DCRM_DX=$CRM_DX -DCRM_DT=$CRM_DT -DPLEV=${PLEV} -DPSUBCOLS=1 -DPCOLS=16 -DHAVE_IEEE_ARITHMETIC"
+[ "$USE_ECPP"  -eq "1" ] && CPPDEFS="${CPPDEFS} -DECPP "
+[ "$CLUBB_CRM" -eq "1" ] && CPPDEFS="${CPPDEFS} -DCLUBB_CRM -DCLUBB_REAL_TYPE=dp "
+
+##########################################################################################
+## Determine the directories to inluce for source files
+##########################################################################################
+echo "$CRM_ROOT"                                              >  Filepath
+echo "$CRM_ROOT/../cam"                                       >> Filepath
+echo "$CRM_ROOT/../../../../../cime/share/csm_share/shr"      >> Filepath
+echo "$CRM_ROOT/../../control"                                >> Filepath
+echo "$CRM_ROOT/../../utils"                                  >> Filepath
+[ "$CLUBB_CRM" -eq "1"  ] && echo "$CRM_ROOT/../clubb"        >> Filepath \
+                          && echo "$CRM_ROOT/SGS_CLUBBkvhkvm" >> Filepath
+[ "$CLUBB_CRM" -ne "1"  ] && echo "$CRM_ROOT/SGS_TKE"         >> Filepath
+[ "$USE_ECPP"  -eq "1"  ] && echo "$CRM_ROOT/../cam/ecpp"     >> Filepath
+[ "$ADV"   == "UM5"     ] && echo "$CRM_ROOT/ADV_UM5"         >> Filepath
+[ "$ADV"   == "MPDATA"  ] && echo "$CRM_ROOT/ADV_MPDATA"      >> Filepath
+[ "$MICRO" == "sam1mom" ] && echo "$CRM_ROOT/MICRO_SAM1MOM"   >> Filepath
+[ "$MICRO" == "m2005"   ] && [ "$USE_CRM_CLDFRAC" ==  "1" ] && echo "$CRM_ROOT/MICRO_M2005FRAC" >> Filepath
+[ "$MICRO" == "m2005"   ] && [ "$USE_CRM_CLDFRAC" -ne "1" ] && echo "$CRM_ROOT/MICRO_M2005"     >> Filepath
+
+##########################################################################################
+## Define the source files based on directories, and copy to build_root
+##########################################################################################
+$SCRIPTS/mkSrcfiles
+for i in `cat Filepath`
+do
+   : 
+   cp -f $i/*.f . >& /dev/null
+   cp -f $i/*.F . >& /dev/null
+   cp -f $i/*.f90 . >& /dev/null
+   cp -f $i/*.F90 . >& /dev/null
+   cp -f $i/*.c . >& /dev/null
+   cp -f $i/*.in . >& /dev/null
+done
+
+##########################################################################################
+## Go ahead and transform the .in files to .F90 files
+##########################################################################################
+for f in *.F90.in
+do
+   :
+   $SCRIPTS/genf90.pl $f > "`basename "$f" .F90.in`.F90"
+done
+
+##########################################################################################
+## Define the dependencies between the source files
+##########################################################################################
+ls *.f *.F *.F90 *.f90 *.c > Srcfiles 2> /dev/null
+echo "." > Filepath
+$SCRIPTS/mkDepends ./Filepath ./Srcfiles > ./Depends
+
+echo "CPPDEFS := ${CPPDEFS}" > make.inc
+
+cp $SCRIPTS/Makefile .
+
+
