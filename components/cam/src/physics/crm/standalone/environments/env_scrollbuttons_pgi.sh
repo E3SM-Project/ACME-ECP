5adae75b28edf5460c5e4930a5a2d9ab996293b7
Matthew R. Norman <normanmr@ornl.gov>
2017-05-26 13:04:20 -0400

    Missing -acc flag in the flags

--- components/cam/src/physics/crm/standalone/environments/env_scrollbuttons_pgi.sh
+++ components/cam/src/physics/crm/standalone/environments/env_scrollbuttons_pgi.sh
@@ -10,8 +10,8 @@ echo "mpif90=`which mpif90`"
 
 export FC=mpif90
 export CC=mpicc
-export FFLAGS="-O2 -ta=tesla,unroll,cuda8.0,cc60,loadcache:L1 -Minfo=accel"
-export CFLAGS="-O2 -ta=tesla,unroll,cuda8.0,cc60,loadcache:L1 -Minfo=accel"
+export FFLAGS="-O2 -acc -ta=tesla,unroll,cuda8.0,cc60,loadcache:L1 -Minfo=accel"
+export CFLAGS="-O2 -acc -ta=tesla,unroll,cuda8.0,cc60,loadcache:L1 -Minfo=accel"
 export FREEFLAGS="-Mextend"
 export FIXEDFLAGS=""
 export LDFLAGS="-L$NETCDF_DIR/lib -lnetcdff -lnetcdf -L$HDF5_DIR/lib -lhdf5_hl -lhdf5 -L$SZIP_DIR/lib -lsz -L$ZLIB_DIR/lib -lz -L$CURL_DIR/lib -lcurl -ldl -ta=tesla,pinned,unroll,cuda8.0,cc60,loadcache:L1"
