fea7125aaa5f7270a790b7cc09c12fac77c3b32b
Benjamin Hillman <benjaminr.hillman@gmail.com>
2018-01-25 16:51:04 -0700

    Add SP-specific tests
    Add tests using the full superparameterization configuration. ECP test
    suite split into a non-sp suite and an sp suite to allow for different
    time limits for tests in the two categories, since SP tests will take
    more time.

--- cime/scripts/lib/update_acme_tests.py
+++ cime/scripts/lib/update_acme_tests.py
@@ -173,11 +173,26 @@ _TEST_SUITES = {
                           "SMS_D_Lm6.1x1_brazil.ICLM45ED")
                          ),
 
-    "acme_ecp"       : (None, None,
-                        ("ERP_Ld31.ne4_oQU240.A_WCYCL2000",
-                         "ERP_Ld5.ne30_ne30.FC5AV1C-L",
-                         "ERP_Ld5.ne4_ne4.FC5AV1C-L")
-                        ),
+    # Tests for ECP (MMF) development
+    #
+    # ECP tests without superparameterization; this is a limited set of tests
+    # meant just to check that basic functionality has not been affected by
+    # development, without running the full acme_developer suite.
+    "acme_ecp_nosp" : (None, None,
+                       ("ERP_Ld31.ne4_oQU240.A_WCYCL2000",
+                        "ERP_Ld5.ne30_ne30.FC5AV1C-L",
+                        "ERP_Ld5.ne4_ne4.FC5AV1C-L")),
+
+    # Superparameterization tests; these tests check for changes in the full ECP
+    # model configuration and test functionality of the superparameterization.
+    "acme_ecp_sp" : (None, "04:00:00",
+                     ("ERP_Ld3_PX.ne16_ne16.FSP1V1-TEST",
+                      "ERP_Ld3_PX.ne16_ne16.FSP2V1-TEST")),
+
+    # Define the acme_ecp subset to be a superset of the ECP tests with and
+    # without superparameterization (check both host model and
+    # superparameterization functionality)
+    "acme_ecp" : (("acme_ecp_nosp", "acme_ecp_sp"), None, ()),
 }
 
 ###############################################################################
