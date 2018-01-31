242aca892800768e8048683ee91598bd403cc8a2
Matthew R. Norman <normanmr@ornl.gov>
2018-01-31 08:02:26 -0500

    commenting out m2005 variables for now

--- components/cam/src/physics/crm/vars.F90
+++ components/cam/src/physics/crm/vars.F90
@@ -193,12 +193,17 @@ module vars
   real(crm_rknd), allocatable :: hgaer(:,:,:) !REDIM    ! hygroscopicity of aerosol mode
 #endif
 
-  integer :: ncondavg, icondavg_cld, icondavg_cor, icondavg_cordn, &
-  icondavg_satdn, icondavg_satup, icondavg_env
-  real(crm_rknd), allocatable :: condavg_factor(:,:) ! replaces cloud_factor, core_factor
-  real(crm_rknd), allocatable :: condavg_mask(:,:,:,:) ! indicator array for various conditional averages
-  character(LEN=8), allocatable :: condavgname(:) ! array of short names
-  character(LEN=25), allocatable :: condavglongname(:) ! array of long names
+!  integer :: ncondavg
+! integer :: icondavg_cld
+! integer :: icondavg_cor
+! integer :: icondavg_cordn
+! integer :: icondavg_satdn
+! integer :: icondavg_satup
+! integer :: icondavg_env
+! real(crm_rknd), allocatable :: condavg_factor(:,:) ! replaces cloud_factor, core_factor
+! real(crm_rknd), allocatable :: condavg_mask(:,:,:,:) ! indicator array for various conditional averages
+! character(LEN=8), allocatable :: condavgname(:) ! array of short names
+! character(LEN=25), allocatable :: condavglongname(:) ! array of long names
 
 contains
 
