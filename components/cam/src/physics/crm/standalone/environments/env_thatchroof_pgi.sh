2ecf56ea03d0f2162f4cf86f096b4d421836bd3e
Matt Norman <normanmr@ornl.gov>
2018-02-06 11:46:07 -0500

    Fixed thatchroof environments

--- components/cam/src/physics/crm/standalone/environments/env_thatchroof_pgi.sh
+++ components/cam/src/physics/crm/standalone/environments/env_thatchroof_pgi.sh
@@ -10,11 +10,11 @@ echo "mpif90=`which mpif90`"
 
 export FC=mpif90
 export CC=mpicc
-export FFLAGS="-O2"
-export CFLAGS="-O2"
+export FFLAGS="-O3 -Mvect=nosse"
+export CFLAGS="-O3"
 export FREEFLAGS="-Mextend"
 export FIXEDFLAGS=""
-export LDFLAGS="-L$NETCDF_DIR/lib -lnetcdff -lnetcdf -L$HDF5_DIR/lib -lhdf5_hl -lhdf5 -L$SZIP_DIR/lib -lsz -L$ZLIB_DIR/lib -lz -ldl -ta=tesla,pinned,unroll,cuda8.0,cc35,loadcache:L1"
+export LDFLAGS="`nf-config --flibs` `nc-config --libs`"
 export INCLUDE="-I$NETCDF_DIR/include"
 
 export CPPDEFS=" -DFORTRANUNDERSCORE "
--- components/cam/src/physics/crm/standalone/environments/env_thatchroof_pgiacc.sh
+++ components/cam/src/physics/crm/standalone/environments/env_thatchroof_pgiacc.sh
@@ -10,7 +10,7 @@ echo "mpif90=`which mpif90`"
 
 export FC=mpif90
 export CC=mpicc
-export FFLAGS="-I$NETCDF_DIR/include -O3 -ta=nvidia,cc50,managed,ptxinfo -Minfo=accel"
+export FFLAGS="-O3 -mvect=nosse -ta=nvidia,cc50,managed,ptxinfo -Minfo=accel"
 export CFLAGS="-O3"
 export FREEFLAGS="-Mextend"
 export FIXEDFLAGS=""
