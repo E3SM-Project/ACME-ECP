0bb10299ed3cbab806919c914f5665dec3db670e
Matt Norman <normanmr@ornl.gov>
2018-02-06 09:14:09 -0500

    Adding pgiacc environment file for standalone

---
+++ components/cam/src/physics/crm/standalone/environments/env_thatchroof_pgiacc.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+source /usr/share/modules/init/bash
+
+module purge
+module load pgi/17.10
+module load netcdf hdf5 szip zlib openmpi/pgi_2.1.2
+
+echo "mpif90=`which mpif90`"
+
+export FC=mpif90
+export CC=mpicc
+export FFLAGS="-I$NETCDF_DIR/include -O2 -ta=nvidia,cc50,managed,ptxinfo -Minfo=accel"
+export CFLAGS="-O2"
+export FREEFLAGS="-Mextend"
+export FIXEDFLAGS=""
+export LDFLAGS="`nf-config --flibs` `nc-config --libs` -ta=nvidia,cc50,managed,ptxinfo"
+export INCLUDE="-I$NETCDF_DIR/include"
+
+export CPPDEFS=" -DFORTRANUNDERSCORE "
+
