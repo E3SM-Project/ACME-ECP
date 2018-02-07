51e17cd0e4061390302ff1fdeee7b43697190326
Matt Norman <normanmr@ornl.gov>
2018-02-07 15:44:24 -0500

    advect_scalar2D is on the GPu

--- components/cam/src/physics/crm/ADV_MPDATA/advect_scalar2D.F90
+++ components/cam/src/physics/crm/ADV_MPDATA/advect_scalar2D.F90
@@ -24,12 +24,8 @@ contains
     real(crm_rknd), allocatable :: mn   (:,:,:,:)
     real(crm_rknd), allocatable :: uuu  (:,:,:,:)
     real(crm_rknd), allocatable :: www  (:,:,:,:)
-    real(crm_rknd), allocatable :: dd   (:)
-    real(crm_rknd), allocatable :: iadz (:,:)
-    real(crm_rknd), allocatable :: irho (:,:)
-    real(crm_rknd), allocatable :: irhow(:,:)
 
-    real(crm_rknd) eps
+    real(crm_rknd) eps, irho
     integer i,j,k,ic,ib,kc,kb,icrm
     logical nonos
     real(crm_rknd) x1, x2, a, b, a1, a2, y
@@ -44,17 +40,18 @@ contains
     allocate( mn   (ncrms,0:nxp1,1,nzm)  )
     allocate( uuu  (ncrms,-1:nxp3,1,nzm) )
     allocate( www  (ncrms,-1:nxp2,1,nz)  )
-    allocate( dd   (ncrms)               )
-    allocate( iadz (ncrms,nzm)           )
-    allocate( irho (ncrms,nzm)           )
-    allocate( irhow(ncrms,nzm)           )
 
     nonos = .true.
     eps = 1.e-10
 
     j=1
 
-    www(:,:,:,nz)=0.
+    !$acc parallel loop gang vector collapse(2)
+    do i = -1,nxp2
+      do icrm = 1 , ncrms
+        www(icrm,i,j,nz)=0.
+      enddo
+    enddo
 
     if(dowallx) then
 
@@ -101,106 +98,101 @@ contains
 
     end if  ! nono
 
+    !$acc parallel loop gang vector collapse(2)
+    do k = 1 , nzm
+      do icrm = 1 , ncrms
+        flux(icrm,k) = 0.
+      enddo
+    enddo
+
+    !$acc parallel loop gang vector collapse(3)
     do k=1,nzm
-      kb=max(1,k-1)
       do i=-1,nxp3
         do icrm = 1 , ncrms
+          kb=max(1,k-1)
           uuu(icrm,i,j,k)=max(real(0.,crm_rknd),u(icrm,i,j,k))*f(icrm,i-1,j,k)+min(real(0.,crm_rknd),u(icrm,i,j,k))*f(icrm,i,j,k)
-        end do
-      end do
-      do i=-1,nxp2
-        do icrm = 1 , ncrms
-          www(icrm,i,j,k)=max(real(0.,crm_rknd),w(icrm,i,j,k))*f(icrm,i,j,kb)+min(real(0.,crm_rknd),w(icrm,i,j,k))*f(icrm,i,j,k)
-        end do
-      end do
-      flux(:,k) = 0.
-      do i=1,nx
-        do icrm = 1 , ncrms
-          flux(icrm,k) = flux(icrm,k) + www(icrm,i,j,k)
+          if (i <= nxp2) www(icrm,i,j,k)=max(real(0.,crm_rknd),w(icrm,i,j,k))*f(icrm,i,j,kb)+min(real(0.,crm_rknd),w(icrm,i,j,k))*f(icrm,i,j,k)
+          if (i >= 1 .and. i <= nx) then
+            !$acc atomic update
+            flux(icrm,k) = flux(icrm,k) + www(icrm,i,j,k)
+          endif
         end do
       end do
     end do
 
+    !$acc parallel loop gang vector collapse(3)
     do k=1,nzm
-      irho(:,k) = 1./rho(:,k)
-      iadz(:,k) = 1./adz(:,k)
       do i=-1,nxp2
         do icrm = 1 , ncrms
-          f(icrm,i,j,k) = f(icrm,i,j,k) - (uuu(icrm,i+1,j,k)-uuu(icrm,i,j,k) &
-          + (www(icrm,i,j,k+1)-www(icrm,i,j,k))*iadz(icrm,k))*irho(icrm,k)
+          f(icrm,i,j,k) = f(icrm,i,j,k) - (uuu(icrm,i+1,j,k)-uuu(icrm,i,j,k) + (www(icrm,i,j,k+1)-www(icrm,i,j,k))/adz(icrm,k))/rho(icrm,k)
         end do
       end do
     end do
 
 
+    !$acc parallel loop gang vector collapse(3)
     do k=1,nzm
-      kc=min(nzm,k+1)
-      kb=max(1,k-1)
-      dd(:)=2./(kc-kb)/adz(:,k)
-      irhow(:,k)=1./(rhow(:,k)*adz(:,k))
       do i=0,nxp2
         do icrm = 1 , ncrms
-          ib=i-1
-          uuu(icrm,i,j,k)=andiff(f(icrm,ib,j,k),f(icrm,i,j,k),u(icrm,i,j,k),irho(icrm,k)) &
-          - across(dd(icrm)*(f(icrm,ib,j,kc)+f(icrm,i,j,kc)-f(icrm,ib,j,kb)-f(icrm,i,j,kb)), &
-          u(icrm,i,j,k), w(icrm,ib,j,k)+w(icrm,ib,j,kc)+w(icrm,i,j,k)+w(icrm,i,j,kc)) *irho(icrm,k)
-        end do
-      end do
-      do i=0,nxp1
-        do icrm = 1 , ncrms
+          kc=min(nzm,k+1)
+          kb=max(1,k-1)
           ib=i-1
           ic=i+1
-          www(icrm,i,j,k)=andiff(f(icrm,i,j,kb),f(icrm,i,j,k),w(icrm,i,j,k),irhow(icrm,k)) &
-          -across(f(icrm,ic,j,kb)+f(icrm,ic,j,k)-f(icrm,ib,j,kb)-f(icrm,ib,j,k), &
-          w(icrm,i,j,k), u(icrm,i,j,kb)+u(icrm,i,j,k)+u(icrm,ic,j,k)+u(icrm,ic,j,kb)) *irho(icrm,k)
+          irho = 1./rho(icrm,k)
+          uuu(icrm,i,j,k)=andiff(f(icrm,ib,j,k),f(icrm,i,j,k),u(icrm,i,j,k),irho) &
+          - across(2./(kc-kb)/adz(icrm,k)*(f(icrm,ib,j,kc)+f(icrm,i,j,kc)-f(icrm,ib,j,kb)-f(icrm,i,j,kb)), &
+          u(icrm,i,j,k), w(icrm,ib,j,k)+w(icrm,ib,j,kc)+w(icrm,i,j,k)+w(icrm,i,j,kc)) *irho
+          if (i <= nxp1) then
+            www(icrm,i,j,k)=andiff(f(icrm,i,j,kb),f(icrm,i,j,k),w(icrm,i,j,k),1./(rhow(icrm,k)*adz(icrm,k))) &
+            -across(f(icrm,ic,j,kb)+f(icrm,ic,j,k)-f(icrm,ib,j,kb)-f(icrm,ib,j,k), &
+            w(icrm,i,j,k), u(icrm,i,j,kb)+u(icrm,i,j,k)+u(icrm,ic,j,k)+u(icrm,ic,j,kb)) / rho(icrm,k)
+          endif
         end do
       end do
     end do
-    www(:,:,:,1) = 0.
+
+    !$acc parallel loop gang vector collapse(2)
+    do i = -1,nxp2
+      do icrm = 1 , ncrms
+        www(icrm,i,j,1)=0.
+      enddo
+    enddo
     !---------- non-osscilatory option ---------------
 
     if(nonos) then
 
+      !$acc parallel loop gang vector collapse(3)
       do k=1,nzm
-        kc=min(nzm,k+1)
-        kb=max(1,k-1)
         do i=0,nxp1
           do icrm = 1 , ncrms
+            kc=min(nzm,k+1)
+            kb=max(1,k-1)
             ib=i-1
             ic=i+1
             mx(icrm,i,j,k)=max(f(icrm,ib,j,k),f(icrm,ic,j,k),f(icrm,i,j,kb),f(icrm,i,j,kc),f(icrm,i,j,k),mx(icrm,i,j,k))
             mn(icrm,i,j,k)=min(f(icrm,ib,j,k),f(icrm,ic,j,k),f(icrm,i,j,kb),f(icrm,i,j,kc),f(icrm,i,j,k),mn(icrm,i,j,k))
-          end do
-        end do
-      end do
-
-      do k=1,nzm
-        kc=min(nzm,k+1)
-        do i=0,nxp1
-          do icrm = 1 , ncrms
-            ic=i+1
             mx(icrm,i,j,k)=rho(icrm,k)*(mx(icrm,i,j,k)-f(icrm,i,j,k))/(pn(uuu(icrm,ic,j,k)) + pp(uuu(icrm,i,j,k))+&
-            iadz(icrm,k)*(pn(www(icrm,i,j,kc)) + pp(www(icrm,i,j,k)))+eps)
+            (pn(www(icrm,i,j,kc)) + pp(www(icrm,i,j,k)))/adz(icrm,k)+eps)
             mn(icrm,i,j,k)=rho(icrm,k)*(f(icrm,i,j,k)-mn(icrm,i,j,k))/(pp(uuu(icrm,ic,j,k)) + pn(uuu(icrm,i,j,k))+&
-            iadz(icrm,k)*(pp(www(icrm,i,j,kc)) + pn(www(icrm,i,j,k)))+eps)
+            (pp(www(icrm,i,j,kc)) + pn(www(icrm,i,j,k)))/adz(icrm,k)+eps)
           end do
         end do
       end do
 
+      !$acc parallel loop gang vector collapse(3)
       do k=1,nzm
-        kb=max(1,k-1)
         do i=1,nxp1
           do icrm = 1 , ncrms
+            kb=max(1,k-1)
             ib=i-1
             uuu(icrm,i,j,k)= pp(uuu(icrm,i,j,k))*min(real(1.,crm_rknd),mx(icrm,i,j,k), mn(icrm,ib,j,k)) &
             - pn(uuu(icrm,i,j,k))*min(real(1.,crm_rknd),mx(icrm,ib,j,k),mn(icrm,i,j,k))
-          end do
-        end do
-        do i=1,nx
-          do icrm = 1 , ncrms
-            www(icrm,i,j,k)= pp(www(icrm,i,j,k))*min(real(1.,crm_rknd),mx(icrm,i,j,k), mn(icrm,i,j,kb)) &
-            - pn(www(icrm,i,j,k))*min(real(1.,crm_rknd),mx(icrm,i,j,kb),mn(icrm,i,j,k))
-            flux(icrm,k) = flux(icrm,k) + www(icrm,i,j,k)
+            if (i <= nx) then
+              www(icrm,i,j,k)= pp(www(icrm,i,j,k))*min(real(1.,crm_rknd),mx(icrm,i,j,k), mn(icrm,i,j,kb)) &
+              - pn(www(icrm,i,j,k))*min(real(1.,crm_rknd),mx(icrm,i,j,kb),mn(icrm,i,j,k))
+              !$acc atomic update
+              flux(icrm,k) = flux(icrm,k) + www(icrm,i,j,k)
+            endif
           end do
         end do
       end do
@@ -209,16 +201,17 @@ contains
     endif ! nonos
 
 
+    !$acc parallel loop gang vector collapse(3)
     do k=1,nzm
-      kc=k+1
       do i=1,nx
         do icrm = 1 , ncrms
+          kc=k+1
           ! MK: added fix for very small negative values (relative to positive values)
           !     especially  when such large numbers as
           !     hydrometeor concentrations are advected. The reason for negative values is
           !     most likely truncation error.
           f(icrm,i,j,k)= max(real(0.,crm_rknd), f(icrm,i,j,k) - (uuu(icrm,i+1,j,k)-uuu(icrm,i,j,k) &
-          +(www(icrm,i,j,k+1)-www(icrm,i,j,k))*iadz(icrm,k))*irho(icrm,k))
+          +(www(icrm,i,j,k+1)-www(icrm,i,j,k))/adz(icrm,k))/rho(icrm,k))
         end do
       end do
     end do
@@ -227,10 +220,6 @@ contains
     deallocate( mn    )
     deallocate( uuu   )
     deallocate( www   )
-    deallocate( dd    )
-    deallocate( iadz  )
-    deallocate( irho  )
-    deallocate( irhow )
 
   end subroutine advect_scalar2D
 
