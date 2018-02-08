981c062ca53c09cbb702d9cc2beda4c44510c657
Matt Norman <normanmr@ornl.gov>
2018-02-08 15:12:04 -0500

    diagnosee is on the GPU

--- components/cam/src/physics/crm/diagnose.F90
+++ components/cam/src/physics/crm/diagnose.F90
@@ -16,12 +16,13 @@ contains
     integer, intent(in) :: ncrms
 
     integer i,j,k,kb,kc,k200(ncrms),k500(ncrms),k850(ncrms),icrm
-    real(8) coef, coef1(ncrms)
+    real(8) coef, coef1(ncrms), tmp
     real(crm_rknd) omn, omp, tmp_lwp
 
     coef = 1./real(nx*ny,crm_rknd)
 
     k200(:) = nzm
+    !$acc parallel loop gang vector collapse(2)
     do k=1,nzm
       do icrm = 1 , ncrms
         u0(icrm,k)=0.
@@ -39,26 +40,49 @@ contains
         if(pres(icrm,kc).le.200..and.pres(icrm,kb).gt.200.) k200(icrm)=k
         coef1(icrm) = rho(icrm,k)*dz(icrm)*adz(icrm,k)*dtfactor
       enddo
+    enddo
+    !$acc parallel loop gang vector collapse(4)
+    do k=1,nzm
       do j=1,ny
         do i=1,nx
           do icrm = 1 , ncrms
             tabs(icrm,i,j,k) = t(icrm,i,j,k)-gamaz(icrm,k)+ fac_cond * (qcl(icrm,i,j,k)+qpl(icrm,i,j,k)) +&
             fac_sub *(qci(icrm,i,j,k) + qpi(icrm,i,j,k))
+            !$acc atomic update
             u0(icrm,k)=u0(icrm,k)+u(icrm,i,j,k)
+            !$acc atomic update
             v0(icrm,k)=v0(icrm,k)+v(icrm,i,j,k)
+            !$acc atomic update
             p0(icrm,k)=p0(icrm,k)+p(icrm,i,j,k)
+            !$acc atomic update
             t0(icrm,k)=t0(icrm,k)+t(icrm,i,j,k)
+            !$acc atomic update
             tabs0(icrm,k)=tabs0(icrm,k)+tabs(icrm,i,j,k)
-            q0(icrm,k)=q0(icrm,k)+qv(icrm,i,j,k)+qcl(icrm,i,j,k)+qci(icrm,i,j,k)
-            qn0(icrm,k) = qn0(icrm,k) + qcl(icrm,i,j,k) + qci(icrm,i,j,k)
-            qp0(icrm,k) = qp0(icrm,k) + qpl(icrm,i,j,k) + qpi(icrm,i,j,k)
+            tmp = qv(icrm,i,j,k)+qcl(icrm,i,j,k)+qci(icrm,i,j,k)
+            !$acc atomic update
+            q0(icrm,k)=q0(icrm,k)+tmp
+            tmp = qcl(icrm,i,j,k) + qci(icrm,i,j,k)
+            !$acc atomic update
+            qn0(icrm,k) = qn0(icrm,k) + tmp
+            tmp = qpl(icrm,i,j,k) + qpi(icrm,i,j,k)
+            !$acc atomic update
+            qp0(icrm,k) = qp0(icrm,k) + tmp
 
-            pw_xy(icrm,i,j) = pw_xy(icrm,i,j)+qv(icrm,i,j,k)*coef1(icrm)
-            cw_xy(icrm,i,j) = cw_xy(icrm,i,j)+qcl(icrm,i,j,k)*coef1(icrm)
-            iw_xy(icrm,i,j) = iw_xy(icrm,i,j)+qci(icrm,i,j,k)*coef1(icrm)
+            tmp = qv (icrm,i,j,k)*coef1(icrm)
+            !$acc atomic update
+            pw_xy(icrm,i,j) = pw_xy(icrm,i,j)+tmp
+            tmp = qcl(icrm,i,j,k)*coef1(icrm)
+            !$acc atomic update
+            cw_xy(icrm,i,j) = cw_xy(icrm,i,j)+tmp
+            tmp = qci(icrm,i,j,k)*coef1(icrm)
+            !$acc atomic update
+            iw_xy(icrm,i,j) = iw_xy(icrm,i,j)+tmp
           end do
         end do
       end do
+    end do
+    !$acc parallel loop gang vector collapse(2)
+    do k=1,nzm
       do icrm = 1 , ncrms
         u0(icrm,k)=u0(icrm,k)*coef
         v0(icrm,k)=v0(icrm,k)*coef
@@ -71,9 +95,10 @@ contains
       end do
     end do ! k
 
-    k500(:) = nzm
-    do k = 1,nzm
-      do icrm = 1 , ncrms
+    !$acc parallel loop gang vector
+    do icrm = 1 , ncrms
+      k500(icrm) = nzm
+      do k = 1,nzm
         kc=min(nzm,k+1)
         if((pres(icrm,kc).le.500.).and.(pres(icrm,k).gt.500.)) then
           if ((500.-pres(icrm,kc)).lt.(pres(icrm,k)-500.))then
@@ -86,6 +111,7 @@ contains
     end do
 
 
+    !$acc parallel loop gang vector collapse(3)
     do j=1,ny
       do i=1,nx
         do icrm = 1 , ncrms
@@ -98,15 +124,21 @@ contains
       end do
     end do
 
-    qv0 = q0 - qn0
+    !$acc parallel loop gang vector collapse(2)
+    do k = 1 , nzm
+      do icrm = 1 , ncrms
+        qv0(icrm,k) = q0 (icrm,k)- qn0(icrm,k)
+      enddo
+    enddo
 
     !=====================================================
     ! UW ADDITIONS
 
     ! FIND VERTICAL INDICES OF 850MB, COMPUTE SWVP
-    k850(:) = 1
-    do k = 1,nzm
-      do icrm = 1 , ncrms
+    !$acc parallel loop gang vector
+    do icrm = 1 , ncrms
+      k850(icrm) = 1
+      do k = 1,nzm
         if(pres(icrm,k).le.850.) then
           k850(icrm) = k
           EXIT
@@ -114,23 +146,23 @@ contains
       end do
     end do
 
+    !$acc parallel loop gang vector collapse(4)
     do k=1,nzm
-      do icrm = 1 , ncrms
-        coef1(icrm) = rho(icrm,k)*dz(icrm)*adz(icrm,k)*dtfactor
-      end do
       do j=1,ny
         do i=1,nx
           do icrm = 1 , ncrms
+            tmp = rho(icrm,k)*dz(icrm)*adz(icrm,k)*dtfactor
             ! Saturated water vapor path with respect to water. Can be used
             ! with water vapor path (= pw) to compute column-average
             ! relative humidity.
-            swvp_xy(icrm,i,j) = swvp_xy(icrm,i,j)+qsatw_crm(tabs(icrm,i,j,k),pres(icrm,k))*coef1(icrm)
+            swvp_xy(icrm,i,j) = swvp_xy(icrm,i,j)+qsatw_crm(tabs(icrm,i,j,k),pres(icrm,k))*tmp
           end do
         end do
       end do
     end do ! k
 
     ! ACCUMULATE AVERAGES OF TWO-DIMENSIONAL STATISTICS
+    !$acc parallel loop gang vector collapse(3)
     do j=1,ny
       do i=1,nx
         do icrm = 1 , ncrms
@@ -150,17 +182,16 @@ contains
     ! WHERE THE PRECIPITATE MIXING RATIO > 0.001 G/KG.
 
     ! initially, zero out heights and set cloudtoptemp to SST
-    do icrm = 1 , ncrms
-      cloudtopheight(icrm,:,:) = 0.
-      cloudtoptemp(icrm,:,:) = sstxy(icrm,1:nx,1:ny)
-      echotopheight(icrm,:,:) = 0.
-    enddo
-    do j = 1,ny
-      do i = 1,nx
-        ! FIND CLOUD TOP HEIGHT
-        tmp_lwp = 0.
-        do k = nzm,1,-1
-          do icrm = 1 , ncrms
+    !$acc parallel loop gang vector collapse(3)
+    do j = 1 , ny
+      do i = 1 , nx
+        do icrm = 1 , ncrms
+          cloudtopheight(icrm,i,j) = 0.
+          cloudtoptemp(icrm,i,j) = sstxy(icrm,i,j)
+          echotopheight(icrm,i,j) = 0.
+          ! FIND CLOUD TOP HEIGHT
+          tmp_lwp = 0.
+          do k = nzm,1,-1
             tmp_lwp = tmp_lwp + (qcl(icrm,i,j,k)+qci(icrm,i,j,k))*rho(icrm,k)*dz(icrm)*adz(icrm,k)
             if (tmp_lwp.gt.0.01) then
               cloudtopheight(icrm,i,j) = z(icrm,k)
@@ -169,10 +200,8 @@ contains
               EXIT
             end if
           end do
-        end do
-        ! FIND ECHO TOP HEIGHT
-        do k = nzm,1,-1
-          do icrm = 1 , ncrms
+          ! FIND ECHO TOP HEIGHT
+          do k = nzm,1,-1
             if (qpl(icrm,i,j,k)+qpi(icrm,i,j,k).gt.1.e-6) then
               echotopheight(icrm,i,j) = z(icrm,k)
               EXIT
