225503cd0569b8bcaaed5584e944eb89009c34c3
Walter Hannah <whannah1@gmail.com>
2018-03-23 16:20:36 -0400

    Added ifdef for SP_CRM_SPLIT outfld calls
    Forgot to add these in the initial implmentation

--- components/cam/src/physics/cam/crm_physics.F90
+++ components/cam/src/physics/cam/crm_physics.F90
@@ -1713,6 +1713,7 @@ end subroutine crm_physics_init
 
 !----------------------------------------------------------------------
 !----------------------------------------------------------------------
+#if defined( SP_CRM_SPLIT )
       !!! diagnostic output for SP_CRM_SPLIT
       if ( phys_stage == 1 ) then 
          call outfld('SPDT1   ',ftem           ,pcols   ,lchnk   )
@@ -1725,6 +1726,7 @@ end subroutine crm_physics_init
          call outfld('SPQPEVP2',qp_evp         ,pcols   ,lchnk   )
          call outfld('SPTLS2  ',t_ls           ,pcols   ,lchnk   )
       end if
+#endif
 
 
        call outfld('SPDT    ',ftem           ,pcols   ,lchnk   )
