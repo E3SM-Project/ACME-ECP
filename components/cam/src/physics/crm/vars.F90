a03b9e4849f3c7aecb686469571c819f1f9b970d
Matt Norman <normanmr@ornl.gov>
2018-05-02 20:24:08 -0400

    Getting vars.F90 ready for allocatables continued

--- components/cam/src/physics/crm/vars.F90
+++ components/cam/src/physics/crm/vars.F90
@@ -150,33 +150,150 @@ module vars
   !----------------------------------------------------------------------
   !	Vertical profiles of quantities sampled for statitistics purposes:
 
-  real(crm_rknd) :: &
-  twle(nz), twsb(nz), precflux(nz), &
-  uwle(nz), uwsb(nz), vwle(nz), vwsb(nz), &
-  radlwup(nz), radlwdn(nz), radswup(nz), radswdn(nz), &
-  radqrlw(nz), radqrsw(nz), w_max, u_max, s_acld, s_acldcold, s_ar, s_arthr, s_sst, &
-  s_acldl, s_acldm, s_acldh,  ncmn, nrmn, z_inv, z_cb, z_ct, z_cbmn, z_ctmn, &
-  z2_inv, z2_cb, z2_ct, cwpmean, cwp2, precmean, prec2, precmax, nrainy, ncloudy, &
-  s_acldisccp, s_acldlisccp, s_acldmisccp, s_acldhisccp, s_ptopisccp, &
-  s_acldmodis, s_acldlmodis, s_acldmmodis, s_acldhmodis, s_ptopmodis, &
-  s_acldmisr, s_ztopmisr, s_relmodis, s_reimodis, s_lwpmodis, s_iwpmodis, &
-  s_tbisccp, s_tbclrisccp, s_acldliqmodis, s_acldicemodis, &
-  s_cldtauisccp,s_cldtaumodis,s_cldtaulmodis,s_cldtauimodis,s_cldalbisccp, &
-  s_flns,s_flnt,s_flntoa,s_flnsc,s_flntoac,s_flds,s_fsns, &
-  s_fsnt,s_fsntoa,s_fsnsc,s_fsntoac,s_fsds,s_solin, &
-  tkeleadv(nz), tkelepress(nz), tkelediss(nz), tkelediff(nz),tkelebuoy(nz), &
-  t2leadv(nz),t2legrad(nz),t2lediff(nz),t2leprec(nz),t2lediss(nz), &
-  q2leadv(nz),q2legrad(nz),q2lediff(nz),q2leprec(nz),q2lediss(nz), &
-  twleadv(nz),twlediff(nz),twlepres(nz),twlebuoy(nz),twleprec(nz), &
-  qwleadv(nz),qwlediff(nz),qwlepres(nz),qwlebuoy(nz),qwleprec(nz), &
-  momleadv(nz,3),momlepress(nz,3),momlebuoy(nz,3), &
-  momlediff(nz,3),tadv(nz),tdiff(nz),tlat(nz), tlatqi(nz),qifall(nz),qpfall(nz)
-  real(crm_rknd) :: tdiff_xy(nz), tdiff_z(nz), ttest0(nzm), ttest1(nz), ttest2(nz,10)  !+++mhwang test
+  real(crm_rknd) :: twle(nz)
+  real(crm_rknd) :: twsb(nz)
+  real(crm_rknd) :: precflux(nz)
+  real(crm_rknd) :: uwle(nz)
+  real(crm_rknd) :: uwsb(nz)
+  real(crm_rknd) :: vwle(nz)
+  real(crm_rknd) :: vwsb(nz)
+  real(crm_rknd) :: radlwup(nz)
+  real(crm_rknd) :: radlwdn(nz)
+  real(crm_rknd) :: radswup(nz)
+  real(crm_rknd) :: radswdn(nz)
+  real(crm_rknd) :: radqrlw(nz)
+  real(crm_rknd) :: radqrsw(nz)
+  real(crm_rknd) :: w_max
+  real(crm_rknd) :: u_max
+  real(crm_rknd) :: s_acld
+  real(crm_rknd) :: s_acldcold
+  real(crm_rknd) :: s_ar
+  real(crm_rknd) :: s_arthr
+  real(crm_rknd) :: s_sst
+  real(crm_rknd) :: s_acldl
+  real(crm_rknd) :: s_acldm
+  real(crm_rknd) :: s_acldh
+  real(crm_rknd) ::  ncmn
+  real(crm_rknd) :: nrmn
+  real(crm_rknd) :: z_inv
+  real(crm_rknd) :: z_cb
+  real(crm_rknd) :: z_ct
+  real(crm_rknd) :: z_cbmn
+  real(crm_rknd) :: z_ctmn
+  real(crm_rknd) :: z2_inv
+  real(crm_rknd) :: z2_cb
+  real(crm_rknd) :: z2_ct
+  real(crm_rknd) :: cwpmean
+  real(crm_rknd) :: cwp2
+  real(crm_rknd) :: precmean
+  real(crm_rknd) :: prec2
+  real(crm_rknd) :: precmax
+  real(crm_rknd) :: nrainy
+  real(crm_rknd) :: ncloudy
+  real(crm_rknd) :: s_acldisccp
+  real(crm_rknd) :: s_acldlisccp
+  real(crm_rknd) :: s_acldmisccp
+  real(crm_rknd) :: s_acldhisccp
+  real(crm_rknd) :: s_ptopisccp
+  real(crm_rknd) :: s_acldmodis
+  real(crm_rknd) :: s_acldlmodis
+  real(crm_rknd) :: s_acldmmodis
+  real(crm_rknd) :: s_acldhmodis
+  real(crm_rknd) :: s_ptopmodis
+  real(crm_rknd) :: s_acldmisr
+  real(crm_rknd) :: s_ztopmisr
+  real(crm_rknd) :: s_relmodis
+  real(crm_rknd) :: s_reimodis
+  real(crm_rknd) :: s_lwpmodis
+  real(crm_rknd) :: s_iwpmodis
+  real(crm_rknd) :: s_tbisccp
+  real(crm_rknd) :: s_tbclrisccp
+  real(crm_rknd) :: s_acldliqmodis
+  real(crm_rknd) :: s_acldicemodis
+  real(crm_rknd) :: s_cldtauisccp,s_cldtaumodis,s_cldtaulmodis,s_cldtauimodis,s_cldalbisccp
+  real(crm_rknd) :: s_flns,s_flnt,s_flntoa,s_flnsc,s_flntoac,s_flds,s_fsns
+  real(crm_rknd) :: s_fsnt,s_fsntoa,s_fsnsc,s_fsntoac,s_fsds,s_solin
+  real(crm_rknd) :: tkeleadv(nz)
+  real(crm_rknd) :: tkelepress(nz)
+  real(crm_rknd) :: tkelediss(nz)
+  real(crm_rknd) :: tkelediff(nz)
+  real(crm_rknd) :: tkelebuoy(nz)
+  real(crm_rknd) :: t2leadv(nz)
+  real(crm_rknd) :: t2legrad(nz)
+  real(crm_rknd) :: t2lediff(nz)
+  real(crm_rknd) :: t2leprec(nz)
+  real(crm_rknd) :: t2lediss(nz)
+  real(crm_rknd) :: q2leadv(nz)
+  real(crm_rknd) :: q2legrad(nz)
+  real(crm_rknd) :: q2lediff(nz)
+  real(crm_rknd) :: q2leprec(nz)
+  real(crm_rknd) :: q2lediss(nz)
+  real(crm_rknd) :: twleadv(nz)
+  real(crm_rknd) :: twlediff(nz)
+  real(crm_rknd) :: twlepres(nz)
+  real(crm_rknd) :: twlebuoy(nz)
+  real(crm_rknd) :: twleprec(nz)
+  real(crm_rknd) :: qwleadv(nz)
+  real(crm_rknd) :: qwlediff(nz)
+  real(crm_rknd) :: qwlepres(nz)
+  real(crm_rknd) :: qwlebuoy(nz)
+  real(crm_rknd) :: qwleprec(nz)
+  real(crm_rknd) :: momleadv(nz,3)
+  real(crm_rknd) :: momlepress(nz,3)
+  real(crm_rknd) :: momlebuoy(nz,3)
+  real(crm_rknd) :: momlediff(nz,3)
+  real(crm_rknd) :: tadv(nz)
+  real(crm_rknd) :: tdiff(nz)
+  real(crm_rknd) :: tlat(nz)
+  real(crm_rknd) :: tlatqi(nz)
+  real(crm_rknd) :: qifall(nz)
+  real(crm_rknd) :: qpfall(nz)
+  real(crm_rknd) :: tdiff_xy(nz)
+  real(crm_rknd) :: tdiff_z(nz)
+  real(crm_rknd) :: ttest0(nzm)
+  real(crm_rknd) :: ttest1(nz)
+  real(crm_rknd) :: ttest2(nz,10)  !+++mhwang test
 
   real(crm_rknd), allocatable :: condavg_factor(:,:) ! replaces cloud_factor, core_factor
   real(crm_rknd), allocatable :: condavg_mask(:,:,:,:) ! indicator array for various conditional averages
   character(LEN=8), allocatable :: condavgname(:) ! array of short names
   character(LEN=25), allocatable :: condavglongname(:) ! array of long names
+
+  real(crm_rknd)   qlsvadv(nzm) ! Large-scale vertical advection tendency for total water
+  real(crm_rknd)   tlsvadv(nzm) ! Large-scale vertical advection tendency for temperature
+  real(crm_rknd)   ulsvadv(nzm) ! Large-scale vertical advection tendency for zonal velocity
+  real(crm_rknd)   vlsvadv(nzm) ! Large-scale vertical advection tendency for meridional velocity
+
+  real(crm_rknd)   qnudge(nzm) ! Nudging of horiz.-averaged total water profile
+  real(crm_rknd)   tnudge(nzm) ! Nudging of horiz.-averaged temperature profile
+  real(crm_rknd)   unudge(nzm) ! Nudging of horiz.-averaged zonal velocity
+  real(crm_rknd)   vnudge(nzm) ! Nudging of horiz.-averaged meridional velocity
+
+  real(crm_rknd)   qstor(nzm) ! Storage of horiz.-averaged total water profile
+  real(crm_rknd)   tstor(nzm) ! Storage of horiz.-averaged temperature profile
+  real(crm_rknd)   ustor(nzm) ! Storage of horiz.-averaged zonal velocity
+  real(crm_rknd)   vstor(nzm) ! Storage of horiz.-averaged meridional velocity
+  real(crm_rknd)   qtostor(nzm) ! Storage of horiz.-averaged total water profile (vapor + liquid)
+
+  real(crm_rknd)   utendcor(nzm) ! coriolis acceleration of zonal velocity
+  real(crm_rknd)   vtendcor(nzm) ! coriolis acceleration of meridional velocity
+
+  real(crm_rknd)   CF3D(1:nx,1:ny,1:nzm)  ! Cloud fraction
+  ! =1.0 when there is no fractional cloudiness scheme
+  ! = cloud fraction produced by fractioal cloudiness scheme when avaiable
+
+  ! 850 mbar horizontal winds
+  real(crm_rknd) u850_xy(nx,ny) ! zonal velocity at 850 mb
+  real(crm_rknd) v850_xy(nx,ny) ! meridional velocity at 850 mb
+
+  ! Surface pressure
+  real(crm_rknd) psfc_xy(nx,ny) ! pressure (in millibar) at lowest grid point
+
+  ! Saturated water vapor path, useful for computing column relative humidity
+  real(crm_rknd) swvp_xy(nx,ny)  ! saturated water vapor path (wrt water)
+
+  ! Cloud and echo top heights, and cloud top temperature (instantaneous)
+  real(crm_rknd) cloudtopheight(nx,ny), echotopheight(nx,ny), cloudtoptemp(nx,ny)
 
   !----------------------------------------------------------------
   ! "observed" (read from snd file) surface characteristics
@@ -224,42 +341,6 @@ module vars
   ! conditional average statistics, subsumes cloud_factor, core_factor, coredn_factor
   integer :: ncondavg, icondavg_cld, icondavg_cor, icondavg_cordn, &
   icondavg_satdn, icondavg_satup, icondavg_env
-
-  real(crm_rknd)   qlsvadv(nzm) ! Large-scale vertical advection tendency for total water
-  real(crm_rknd)   tlsvadv(nzm) ! Large-scale vertical advection tendency for temperature
-  real(crm_rknd)   ulsvadv(nzm) ! Large-scale vertical advection tendency for zonal velocity
-  real(crm_rknd)   vlsvadv(nzm) ! Large-scale vertical advection tendency for meridional velocity
-
-  real(crm_rknd)   qnudge(nzm) ! Nudging of horiz.-averaged total water profile
-  real(crm_rknd)   tnudge(nzm) ! Nudging of horiz.-averaged temperature profile
-  real(crm_rknd)   unudge(nzm) ! Nudging of horiz.-averaged zonal velocity
-  real(crm_rknd)   vnudge(nzm) ! Nudging of horiz.-averaged meridional velocity
-
-  real(crm_rknd)   qstor(nzm) ! Storage of horiz.-averaged total water profile
-  real(crm_rknd)   tstor(nzm) ! Storage of horiz.-averaged temperature profile
-  real(crm_rknd)   ustor(nzm) ! Storage of horiz.-averaged zonal velocity
-  real(crm_rknd)   vstor(nzm) ! Storage of horiz.-averaged meridional velocity
-  real(crm_rknd)   qtostor(nzm) ! Storage of horiz.-averaged total water profile (vapor + liquid)
-
-  real(crm_rknd)   utendcor(nzm) ! coriolis acceleration of zonal velocity
-  real(crm_rknd)   vtendcor(nzm) ! coriolis acceleration of meridional velocity
-
-  real(crm_rknd)   CF3D(1:nx,1:ny,1:nzm)  ! Cloud fraction
-  ! =1.0 when there is no fractional cloudiness scheme
-  ! = cloud fraction produced by fractioal cloudiness scheme when avaiable
-
-  ! 850 mbar horizontal winds
-  real(crm_rknd) u850_xy(nx,ny) ! zonal velocity at 850 mb
-  real(crm_rknd) v850_xy(nx,ny) ! meridional velocity at 850 mb
-
-  ! Surface pressure
-  real(crm_rknd) psfc_xy(nx,ny) ! pressure (in millibar) at lowest grid point
-
-  ! Saturated water vapor path, useful for computing column relative humidity
-  real(crm_rknd) swvp_xy(nx,ny)  ! saturated water vapor path (wrt water)
-
-  ! Cloud and echo top heights, and cloud top temperature (instantaneous)
-  real(crm_rknd) cloudtopheight(nx,ny), echotopheight(nx,ny), cloudtoptemp(nx,ny)
 
   ! END UW ADDITIONS
   !===========================================================================
