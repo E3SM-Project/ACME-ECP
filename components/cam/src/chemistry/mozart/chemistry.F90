6e07313cb76bb922dc0c6f79ee80b5a6365dd474
Benjamin Hillman <benjaminr.hillman@gmail.com>
2018-06-29 13:31:30 -0600

    Simplify calls to radiation_tend
    Simplify calls to radiation_tend by removing arguments passing
    individual land surface and flux fields in and out of radiation_tend.
    Because cam_in is already passed, explicitly passing cam_in%landfrac,
    cam_in%icefrac, and cam_in%snowh is redundant, and these fields are not
    even used within radiation_tend anymore anyways. The surface and TOA
    flux fields are removed from this call as well by moving these fields
    from comsrf (which should be eliminated in the future) to the physics
    buffer: since the purpose of comsrf seems to be to hold some fields
    needed by different modules, exactly what physics_buffer is engineered
    to do, this seems like a logical change and removed duplicate code to
    read and write these fields to restart files. Instead, reading and
    writing the flux information is now handled automatically by the
    physics_buffer interface by declaring the scope of flux fields as
    "global" when adding them to the physics buffer.
    
    The landm land fraction ramp variable is also removed from these calls,
    as it is not actually used within radiation_tend anymore other than to
    pass along to radiation_data to output radiation fields for offline
    calculations. For the time being then, landm is kept in comsrf, but read
    directly from that module data by the radiation_data module.

--- components/cam/src/chemistry/mozart/chemistry.F90
+++ components/cam/src/chemistry/mozart/chemistry.F90
@@ -1334,7 +1334,7 @@ end function chem_is_active
 
   end subroutine chem_timestep_init
 
-  subroutine chem_timestep_tend( state, ptend, cam_in, cam_out, dt, pbuf,  fh2o, fsds )
+  subroutine chem_timestep_tend( state, ptend, cam_in, cam_out, dt, pbuf,  fh2o)
 
 !----------------------------------------------------------------------- 
 ! 
@@ -1349,7 +1349,7 @@ end function chem_is_active
 ! 
 !-----------------------------------------------------------------------
 
-    use physics_buffer,      only : physics_buffer_desc, pbuf_get_field, pbuf_old_tim_idx
+    use physics_buffer,      only : physics_buffer_desc, pbuf_get_field, pbuf_old_tim_idx, pbuf_get_index
     use cam_history,         only : outfld
     use time_manager,        only : get_curr_calday
     use chem_mods,           only : gas_pcnst
@@ -1377,7 +1377,6 @@ end function chem_is_active
     
 
     type(physics_buffer_desc), pointer :: pbuf(:)
-    real(r8),            intent(in)    :: fsds(pcols)     ! longwave down at sfc
 
 !-----------------------------------------------------------------------
 ! Local variables
@@ -1397,6 +1396,7 @@ end function chem_is_active
     real(r8), pointer :: cmfdqr(:,:)
     real(r8), pointer :: nevapr(:,:)
     real(r8), pointer :: cldtop(:)
+    real(r8), pointer :: fsds(:)     ! longwave down at sfc
 
     integer :: tim_ndx
 
@@ -1456,6 +1456,7 @@ end function chem_is_active
     end do
 
     call t_startf( 'chemdr' )
+    call pbuf_get_field(pbuf, pbuf_get_index('FSDS'), fsds)
     call gas_phase_chemdr(lchnk, ncol, imozart, state%q, &
                           state%phis, state%zm, state%zi, calday, &
                           state%t, state%pmid, state%pdel, state%pint, &
