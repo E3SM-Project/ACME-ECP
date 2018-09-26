e4a5739fb3930569ace57288a5671becaa47787f
Matt Norman <normanmr@ornl.gov>
2018-09-26 11:19:22 -0400

    kurant is now refactored and ready for GPU directives. kurant_sgs as well.

--- components/cam/src/physics/crm/kurant.F90
+++ components/cam/src/physics/crm/kurant.F90
@@ -11,41 +11,60 @@ module kurant_mod
       implicit none
       integer, intent(in) :: ncrms
       integer i, j, k, ncycle1(1),ncycle2(1),icrm
-      real(crm_rknd) wm(nz)  ! maximum vertical wind velocity
-      real(crm_rknd) uhm(nz) ! maximum horizontal wind velocity
-      real(crm_rknd) cfl, cfl_sgs
+      real(crm_rknd) wm(nz,ncrms)  ! maximum vertical wind velocity
+      real(crm_rknd) uhm(nz,ncrms) ! maximum horizontal wind velocity
+      real(crm_rknd) cfl, cfl_sgs, tmp
       ncycle = 1
 
       do icrm = 1 , ncrms
-        wm(nz)=0.
+        do k = 1 , nz
+          wm (k,icrm) = 0.
+          uhm(k,icrm) = 0.
+        enddo
+      enddo
+
+      do icrm = 1 , ncrms
         do k = 1,nzm
-          wm(k) = maxval(abs(w(1:nx,1:ny,k,icrm)))
-          uhm(k) = sqrt(maxval(u(1:nx,1:ny,k,icrm)**2+YES3D*v(1:nx,1:ny,k,icrm)**2))
-        end do
-        w_max(icrm)=max( w_max(icrm), real(maxval(w(1:nx,1:ny,1:nz,icrm)),kind(w_max(icrm))) )
-        u_max(icrm)=max( u_max(icrm), real(maxval(uhm(1:nzm))       ,kind(u_max(icrm))) )
+          do j = 1 , ny
+            do i = 1 , nx
+              tmp = abs(w(i,j,k,icrm))
+              !$acc atomic update
+              wm(k,icrm) = max( wm(k,icrm) , tmp )
+              !$acc atomic update
+              w_max(icrm) = max( w_max(icrm) , tmp )
 
-        cfl = 0.
+              tmp = sqrt(u(i,j,k,icrm)**2+YES3D*v(i,j,k,icrm)**2)
+              !$acc atomic update
+              uhm(k,icrm) = max( uhm(k,icrm) , tmp )
+              !$acc atomic update
+              u_max(icrm) = max( u_max(icrm) , tmp )
+            enddo
+          enddo
+        enddo
+      enddo
+
+      cfl = 0.
+      do icrm = 1 , ncrms
         do k=1,nzm
-          cfl = max(cfl,uhm(k)*dt*sqrt((1./dx)**2+YES3D*(1./dy)**2), &
-          max(wm(k),wm(k+1))*dt/(dz(icrm)*adzw(k,icrm)) )
+          tmp = max( uhm(k,icrm)*dt*sqrt((1./dx)**2+YES3D*(1./dy)**2) , max(wm(k,icrm),wm(k+1,icrm))*dt/(dz(icrm)*adzw(k,icrm)) )
+          !$acc atomic update
+          cfl = max( cfl , tmp )
         end do
+      end do
 
-        call kurant_sgs(ncrms,icrm,cfl_sgs)
-        cfl = max(cfl,cfl_sgs)
+      call kurant_sgs(ncrms,cfl)
+      ncycle = max(ncycle,max(1,ceiling(cfl/0.7)))
 
-        ncycle = max(ncycle,max(1,ceiling(cfl/0.7)))
-
-        if(ncycle.gt.4) then
-          if(masterproc) print *,'kurant() - the number of cycles exceeded 4.'
+      if(ncycle.gt.4) then
+        if(masterproc) print *,'kurant() - the number of cycles exceeded 4.'
+        do icrm = 1 , ncrms
           write(0, 5550) cfl, cfl_sgs, latitude(1,1,icrm), longitude(1,1,icrm)
           do k=1, nzm
-            write(0, 5551) k, wm(k), uhm(k), tabs(1,1,k,icrm)
+            write(0, 5551) k, wm(k,icrm), uhm(k,icrm), tabs(1,1,k,icrm)
           end do
-
-          call task_abort()
-        end if
-      enddo
+        end do
+        call task_abort()
+      end if
 
 5550 format('kurant() - cfl: ',f12.2,'  cfl_sgs: ',f12.2,'  lat: ',f6.2,'  lon: ',f6.2)
 5551 format('k: ',i5,'  wm: ',f10.2,'  uhm: ',f10.2,'  tabs: ',f8.2)
