3a7b65e3836b9920121bfe43e22e02df03cf618d
Matt Norman <normanmr@ornl.gov>
2018-09-27 13:33:17 -0400

    diffuse_mom3d refactored

--- components/cam/src/physics/crm/SGS_TKE/diffuse_mom3D.F90
+++ components/cam/src/physics/crm/SGS_TKE/diffuse_mom3D.F90
@@ -21,140 +21,162 @@ contains
     real(crm_rknd) dxy,dxz,dyx,dyz,dzx,dzy
     integer i,j,k,ic,ib,jb,jc,kc,kcu,icrm
     real(crm_rknd) tkx, tky, tkz, rhoi, iadzw, iadz
-    real(crm_rknd) :: fu(0:nx,0:ny,nz)
-    real(crm_rknd) :: fv(0:nx,0:ny,nz)
-    real(crm_rknd) :: fw(0:nx,0:ny,nz)
+    real(crm_rknd) :: fu(0:nx,0:ny,nz,ncrms)
+    real(crm_rknd) :: fv(0:nx,0:ny,nz,ncrms)
+    real(crm_rknd) :: fw(0:nx,0:ny,nz,ncrms)
+
+    rdx2=1./(dx*dx)
+    rdy2=1./(dy*dy)
+    rdx25=0.25*rdx2
+    rdy25=0.25*rdy2
+    dxy=dx/dy
+    dyx=dy/dx
 
     do icrm = 1 , ncrms
-      rdx2=1./(dx*dx)
-      rdy2=1./(dy*dy)
-
-      rdx25=0.25*rdx2
-      rdy25=0.25*rdy2
-
-      dxy=dx/dy
-      dxz=dx/dz(icrm)
-      dyx=dy/dx
-      dyz=dy/dz(icrm)
-
       do k=1,nzm
-        kc=k+1
-        kcu=min(kc,nzm)
-        dxz=dx/(dz(icrm)*adzw(kc,icrm))
-        dyz=dy/(dz(icrm)*adzw(kc,icrm))
-        rdx21=rdx2    * grdf_x(k,icrm)
-        rdy21=rdy2    * grdf_y(k,icrm)
-        rdx251=rdx25  * grdf_x(k,icrm)
-        rdy251=rdy25  * grdf_y(k,icrm)
         do j=1,ny
-          jb=j-1
           do i=0,nx
+            jb=j-1
+            kc=k+1
+            kcu=min(kc,nzm)
             ic=i+1
+            dxz=dx/(dz(icrm)*adzw(kc,icrm))
+            rdx21=rdx2    * grdf_x(k,icrm)
+            rdx251=rdx25  * grdf_x(k,icrm)
             tkx=rdx21*tk(i,j,k,icrm)
-            fu(i,j,k)=-2.*tkx*(u(ic,j,k,icrm)-u(i,j,k,icrm))
+            fu(i,j,k,icrm)=-2.*tkx*(u(ic,j,k,icrm)-u(i,j,k,icrm))
             tkx=rdx251*(tk(i,j,k,icrm)+tk(i,jb,k,icrm)+tk(ic,j,k,icrm)+tk(ic,jb,k,icrm))
-            fv(i,j,k)=-tkx*(v(ic,j,k,icrm)-v(i,j,k,icrm)+(u(ic,j,k,icrm)-u(ic,jb,k,icrm))*dxy)
+            fv(i,j,k,icrm)=-tkx*(v(ic,j,k,icrm)-v(i,j,k,icrm)+(u(ic,j,k,icrm)-u(ic,jb,k,icrm))*dxy)
             tkx=rdx251*(tk(i,j,k,icrm)+tk(ic,j,k,icrm)+tk(i,j,kcu,icrm)+tk(ic,j,kcu,icrm))
-            fw(i,j,k)=-tkx*(w(ic,j,kc,icrm)-w(i,j,kc,icrm)+(u(ic,j,kcu,icrm)-u(ic,j,k,icrm))*dxz)
-          end do
+            fw(i,j,k,icrm)=-tkx*(w(ic,j,kc,icrm)-w(i,j,kc,icrm)+(u(ic,j,kcu,icrm)-u(ic,j,k,icrm))*dxz)
+          enddo
+        enddo
+      enddo
+    enddo
+    do icrm = 1 , ncrms
+      do k=1,nzm
+        do j=1,ny
           do i=1,nx
+            kc=k+1
             ib=i-1
-            dudt(i,j,k,na(icrm),icrm)=dudt(i,j,k,na(icrm),icrm)-(fu(i,j,k)-fu(ib,j,k))
-            dvdt(i,j,k,na(icrm),icrm)=dvdt(i,j,k,na(icrm),icrm)-(fv(i,j,k)-fv(ib,j,k))
-            dwdt(i,j,kc,na(icrm),icrm)=dwdt(i,j,kc,na(icrm),icrm)-(fw(i,j,k)-fw(ib,j,k))
-          end do
-        end do
+            dudt(i,j,k,na(icrm),icrm)=dudt(i,j,k,na(icrm),icrm)-(fu(i,j,k,icrm)-fu(ib,j,k,icrm))
+            dvdt(i,j,k,na(icrm),icrm)=dvdt(i,j,k,na(icrm),icrm)-(fv(i,j,k,icrm)-fv(ib,j,k,icrm))
+            dwdt(i,j,kc,na(icrm),icrm)=dwdt(i,j,kc,na(icrm),icrm)-(fw(i,j,k,icrm)-fw(ib,j,k,icrm))
+          enddo
+        enddo
+      enddo
+    enddo
 
+    do icrm = 1 , ncrms
+      do k=1,nzm
         do j=0,ny
-          jc=j+1
           do i=1,nx
+            jc=j+1
+            kc=k+1
+            kcu=min(kc,nzm)
             ib=i-1
+            dyz=dy/(dz(icrm)*adzw(kc,icrm))
+            rdy21=rdy2    * grdf_y(k,icrm)
+            rdy251=rdy25  * grdf_y(k,icrm)
             tky=rdy21*tk(i,j,k,icrm)
-            fv(i,j,k)=-2.*tky*(v(i,jc,k,icrm)-v(i,j,k,icrm))
+            fv(i,j,k,icrm)=-2.*tky*(v(i,jc,k,icrm)-v(i,j,k,icrm))
             tky=rdy251*(tk(i,j,k,icrm)+tk(ib,j,k,icrm)+tk(i,jc,k,icrm)+tk(ib,jc,k,icrm))
-            fu(i,j,k)=-tky*(u(i,jc,k,icrm)-u(i,j,k,icrm)+(v(i,jc,k,icrm)-v(ib,jc,k,icrm))*dyx)
+            fu(i,j,k,icrm)=-tky*(u(i,jc,k,icrm)-u(i,j,k,icrm)+(v(i,jc,k,icrm)-v(ib,jc,k,icrm))*dyx)
             tky=rdy251*(tk(i,j,k,icrm)+tk(i,jc,k,icrm)+tk(i,j,kcu,icrm)+tk(i,jc,kcu,icrm))
-            fw(i,j,k)=-tky*(w(i,jc,kc,icrm)-w(i,j,kc,icrm)+(v(i,jc,kcu,icrm)-v(i,jc,k,icrm))*dyz)
-          end do
-        end do
+            fw(i,j,k,icrm)=-tky*(w(i,jc,kc,icrm)-w(i,j,kc,icrm)+(v(i,jc,kcu,icrm)-v(i,jc,k,icrm))*dyz)
+          enddo
+        enddo
+      enddo
+    enddo
+    do icrm = 1 , ncrms
+      do k=1,nzm
         do j=1,ny
-          jb=j-1
           do i=1,nx
-            dudt(i,j,k,na(icrm),icrm)=dudt(i,j,k,na(icrm),icrm)-(fu(i,j,k)-fu(i,jb,k))
-            dvdt(i,j,k,na(icrm),icrm)=dvdt(i,j,k,na(icrm),icrm)-(fv(i,j,k)-fv(i,jb,k))
-            dwdt(i,j,kc,na(icrm),icrm)=dwdt(i,j,kc,na(icrm),icrm)-(fw(i,j,k)-fw(i,jb,k))
-          end do
-        end do
+            jb=j-1
+            kc=k+1
+            dudt(i,j,k,na(icrm),icrm)=dudt(i,j,k,na(icrm),icrm)-(fu(i,j,k,icrm)-fu(i,jb,k,icrm))
+            dvdt(i,j,k,na(icrm),icrm)=dvdt(i,j,k,na(icrm),icrm)-(fv(i,j,k,icrm)-fv(i,jb,k,icrm))
+            dwdt(i,j,kc,na(icrm),icrm)=dwdt(i,j,kc,na(icrm),icrm)-(fw(i,j,k,icrm)-fw(i,jb,k,icrm))
+          enddo
+        enddo
+      enddo
+    enddo
 
-      end do
+    do icrm = 1 , ncrms
+      do k = 1 , nzm
+        uwsb(k,icrm)=0.
+        vwsb(k,icrm)=0.
+      enddo
+    enddo
 
-      !-------------------------
-      rdz=1./dz(icrm)
-      dzx=dz(icrm)/dx
-      dzy=dz(icrm)/dy
-
+    !-------------------------
+    do icrm = 1 , ncrms
       do k=1,nzm-1
-        kc=k+1
-        uwsb(kc,icrm)=0.
-        vwsb(kc,icrm)=0.
-        iadz = 1./adz(k,icrm)
-        iadzw= 1./adzw(kc,icrm)
-        rdz2 = rdz*rdz * grdf_z(k,icrm)
-        rdz25 = 0.25*rdz2
         do j=1,ny
-          jb=j-1
           do i=1,nx
+            jb=j-1
+            kc=k+1
             ib=i-1
+            rdz=1./dz(icrm)
+            rdz2 = rdz*rdz * grdf_z(k,icrm)
+            rdz25 = 0.25*rdz2
+            iadz = 1./adz(k,icrm)
+            iadzw= 1./adzw(kc,icrm)
+            dzx=dz(icrm)/dx
+            dzy=dz(icrm)/dy
             tkz=rdz2*tk(i,j,k,icrm)
-            fw(i,j,kc)=-2.*tkz*(w(i,j,kc,icrm)-w(i,j,k,icrm))*rho(k,icrm)*iadz
+            fw(i,j,kc,icrm)=-2.*tkz*(w(i,j,kc,icrm)-w(i,j,k,icrm))*rho(k,icrm)*iadz
             tkz=rdz25*(tk(i,j,k,icrm)+tk(ib,j,k,icrm)+tk(i,j,kc,icrm)+tk(ib,j,kc,icrm))
-            fu(i,j,kc)=-tkz*( (u(i,j,kc,icrm)-u(i,j,k,icrm))*iadzw + &
-            (w(i,j,kc,icrm)-w(ib,j,kc,icrm))*dzx)*rhow(kc,icrm)
+            fu(i,j,kc,icrm)=-tkz*( (u(i,j,kc,icrm)-u(i,j,k,icrm))*iadzw + (w(i,j,kc,icrm)-w(ib,j,kc,icrm))*dzx)*rhow(kc,icrm)
             tkz=rdz25*(tk(i,j,k,icrm)+tk(i,jb,k,icrm)+tk(i,j,kc,icrm)+tk(i,jb,kc,icrm))
-            fv(i,j,kc)=-tkz*( (v(i,j,kc,icrm)-v(i,j,k,icrm))*iadzw + &
-            (w(i,j,kc,icrm)-w(i,jb,kc,icrm))*dzy)*rhow(kc,icrm)
-            uwsb(kc,icrm)=uwsb(kc,icrm)+fu(i,j,kc)
-            vwsb(kc,icrm)=vwsb(kc,icrm)+fv(i,j,kc)
-          end do
-        end do
-      end do
+            fv(i,j,kc,icrm)=-tkz*( (v(i,j,kc,icrm)-v(i,j,k,icrm))*iadzw + (w(i,j,kc,icrm)-w(i,jb,kc,icrm))*dzy)*rhow(kc,icrm)
+            uwsb(kc,icrm)=uwsb(kc,icrm)+fu(i,j,kc,icrm)
+            vwsb(kc,icrm)=vwsb(kc,icrm)+fv(i,j,kc,icrm)
+          enddo
+        enddo
+      enddo
+    enddo
 
-      uwsb(1,icrm) = 0.
-      vwsb(1,icrm) = 0.
-
+    do icrm = 1 , ncrms
       do j=1,ny
         do i=1,nx
+          rdz=1./dz(icrm)
+          rdz2 = rdz*rdz * grdf_z(k,icrm)
           tkz=rdz2*grdf_z(nzm,icrm)*tk(i,j,nzm,icrm)
-          fw(i,j,nz)=-2.*tkz*(w(i,j,nz,icrm)-w(i,j,nzm,icrm))/adz(nzm,icrm)*rho(nzm,icrm)
-          fu(i,j,1)=fluxbu(i,j,icrm) * rdz * rhow(1,icrm)
-          fv(i,j,1)=fluxbv(i,j,icrm) * rdz * rhow(1,icrm)
-          fu(i,j,nz)=fluxtu(i,j,icrm) * rdz * rhow(nz,icrm)
-          fv(i,j,nz)=fluxtv(i,j,icrm) * rdz * rhow(nz,icrm)
-          uwsb(1,icrm) = uwsb(1,icrm) + fu(i,j,1)
-          vwsb(1,icrm) = vwsb(1,icrm) + fv(i,j,1)
-        end do
-      end do
+          fw(i,j,nz,icrm)=-2.*tkz*(w(i,j,nz,icrm)-w(i,j,nzm,icrm))/adz(nzm,icrm)*rho(nzm,icrm)
+          fu(i,j,1,icrm)=fluxbu(i,j,icrm) * rdz * rhow(1,icrm)
+          fv(i,j,1,icrm)=fluxbv(i,j,icrm) * rdz * rhow(1,icrm)
+          fu(i,j,nz,icrm)=fluxtu(i,j,icrm) * rdz * rhow(nz,icrm)
+          fv(i,j,nz,icrm)=fluxtv(i,j,icrm) * rdz * rhow(nz,icrm)
+          uwsb(1,icrm) = uwsb(1,icrm) + fu(i,j,1,icrm)
+          vwsb(1,icrm) = vwsb(1,icrm) + fv(i,j,1,icrm)
+        enddo
+      enddo
+    enddo
 
+    do icrm = 1 , ncrms
       do k=1,nzm
-        kc=k+1
-        rhoi = 1./(rho(k,icrm)*adz(k,icrm))
         do j=1,ny
           do i=1,nx
-            dudt(i,j,k,na(icrm),icrm)=dudt(i,j,k,na(icrm),icrm)-(fu(i,j,kc)-fu(i,j,k))*rhoi
-            dvdt(i,j,k,na(icrm),icrm)=dvdt(i,j,k,na(icrm),icrm)-(fv(i,j,kc)-fv(i,j,k))*rhoi
-          end do
-        end do
-      end do ! k
+            kc=k+1
+            rhoi = 1./(rho(k,icrm)*adz(k,icrm))
+            dudt(i,j,k,na(icrm),icrm)=dudt(i,j,k,na(icrm),icrm)-(fu(i,j,kc,icrm)-fu(i,j,k,icrm))*rhoi
+            dvdt(i,j,k,na(icrm),icrm)=dvdt(i,j,k,na(icrm),icrm)-(fv(i,j,kc,icrm)-fv(i,j,k,icrm))*rhoi
+          enddo
+        enddo
+      enddo ! k
+    enddo
 
+    do icrm = 1 , ncrms
       do k=2,nzm
-        rhoi = 1./(rhow(k,icrm)*adzw(k,icrm))
         do j=1,ny
           do i=1,nx
-            dwdt(i,j,k,na(icrm),icrm)=dwdt(i,j,k,na(icrm),icrm)-(fw(i,j,k+1)-fw(i,j,k))*rhoi
-          end do
-        end do
-      end do ! k
-    end do ! k
+            rhoi = 1./(rhow(k,icrm)*adzw(k,icrm))
+            dwdt(i,j,k,na(icrm),icrm)=dwdt(i,j,k,na(icrm),icrm)-(fw(i,j,k+1,icrm)-fw(i,j,k,icrm))*rhoi
+          enddo
+        enddo
+      enddo ! k
+    enddo
 
   end subroutine diffuse_mom3D
 
