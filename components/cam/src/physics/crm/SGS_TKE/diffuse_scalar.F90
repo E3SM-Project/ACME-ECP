abefd26a4c65330d28cba0d310147491663589ca
Matt Norman <normanmr@ornl.gov>
2018-10-01 10:12:54 -0400

    pushed ncrms into diffuse_scalar

--- components/cam/src/physics/crm/SGS_TKE/diffuse_scalar.F90
+++ components/cam/src/physics/crm/SGS_TKE/diffuse_scalar.F90
@@ -5,47 +5,47 @@ module diffuse_scalar_mod
 
 contains
 
-  subroutine diffuse_scalar (ncrms,icrm,dimx1_d,dimx2_d,dimy1_d,dimy2_d,grdf_x,grdf_y,grdf_z,tkh,f,fluxb,fluxt,fdiff,flux)
+  subroutine diffuse_scalar (ncrms,dimx1_d,dimx2_d,dimy1_d,dimy2_d,grdf_x,grdf_y,grdf_z,tkh,f,fluxb,fluxt,fdiff,flux)
     use grid
     use vars, only: rho, rhow
     use params
     implicit none
-    integer, intent(in) :: ncrms,icrm
+    integer, intent(in) :: ncrms
     ! input:
     integer :: dimx1_d,dimx2_d,dimy1_d,dimy2_d
     real(crm_rknd) grdf_x(nzm,ncrms)! grid factor for eddy diffusion in x
     real(crm_rknd) grdf_y(nzm,ncrms)! grid factor for eddy diffusion in y
     real(crm_rknd) grdf_z(nzm,ncrms)! grid factor for eddy diffusion in z
     real(crm_rknd) tkh (dimx1_d:dimx2_d, dimy1_d:dimy2_d, nzm,ncrms) ! SGS eddy conductivity
-    real(crm_rknd) f(dimx1_s:dimx2_s, dimy1_s:dimy2_s, nzm) ! scalar
-    real(crm_rknd) fluxb(nx,ny)   ! bottom flux
-    real(crm_rknd) fluxt(nx,ny)   ! top flux
-    real(crm_rknd) fdiff(nz)
-    real(crm_rknd) flux(nz)
+    real(crm_rknd) f(dimx1_s:dimx2_s, dimy1_s:dimy2_s, nzm, ncrms) ! scalar
+    real(crm_rknd) fluxb(nx,ny,ncrms)   ! bottom flux
+    real(crm_rknd) fluxt(nx,ny,ncrms)   ! top flux
+    real(crm_rknd) fdiff(nz,ncrms)
+    real(crm_rknd) flux (nz,ncrms)
     ! Local
-    real(crm_rknd) df(dimx1_s:dimx2_s, dimy1_s:dimy2_s, nzm)  ! scalar
-    integer i,j,k
+    real(crm_rknd) df(dimx1_s:dimx2_s, dimy1_s:dimy2_s, nzm,ncrms)  ! scalar
+    integer i,j,k,icrm
 
-    !call t_startf ('diffuse_scalars')
+    do icrm = 1 , ncrms
 
-    df(:,:,:) = f(:,:,:)
+    df(:,:,:,icrm) = f(:,:,:,icrm)
 
     if(RUN3D) then
-      call diffuse_scalar3D (ncrms,icrm,dimx1_d,dimx2_d,dimy1_d,dimy2_d,grdf_x,grdf_y,grdf_z,f,fluxb,fluxt,tkh,rho,rhow,flux)
+      call diffuse_scalar3D (ncrms,icrm,dimx1_d,dimx2_d,dimy1_d,dimy2_d,grdf_x,grdf_y,grdf_z,f(:,:,:,icrm),fluxb(:,:,icrm),fluxt(:,:,icrm),tkh,rho,rhow,flux(:,icrm))
     else
-      call diffuse_scalar2D (ncrms,icrm,dimx1_d,dimx2_d,dimy1_d,dimy2_d,grdf_x,grdf_y,       f,fluxb,fluxt,tkh,rho,rhow,flux)
+      call diffuse_scalar2D (ncrms,icrm,dimx1_d,dimx2_d,dimy1_d,dimy2_d,grdf_x,grdf_y,       f(:,:,:,icrm),fluxb(:,:,icrm),fluxt(:,:,icrm),tkh,rho,rhow,flux(:,icrm))
     endif
 
     do k=1,nzm
-      fdiff(k)=0.
+      fdiff(k,icrm)=0.
       do j=1,ny
         do i=1,nx
-          fdiff(k)=fdiff(k)+f(i,j,k)-df(i,j,k)
+          fdiff(k,icrm)=fdiff(k,icrm)+f(i,j,k,icrm)-df(i,j,k,icrm)
         end do
       end do
     end do
 
-    !call t_stopf ('diffuse_scalars')
+    enddo
 
   end subroutine diffuse_scalar
 
