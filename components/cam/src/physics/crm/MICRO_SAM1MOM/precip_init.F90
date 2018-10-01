b9a63e84e11482cf30df18d4a5d37999ee0368f4
Matt Norman <normanmr@ornl.gov>
2018-10-01 13:08:17 -0400

    precip_init refactored

--- components/cam/src/physics/crm/MICRO_SAM1MOM/precip_init.F90
+++ components/cam/src/physics/crm/MICRO_SAM1MOM/precip_init.F90
@@ -4,21 +4,18 @@ module precip_init_mod
 
 contains
 
-  subroutine precip_init(ncrms,icrm)
-
+  subroutine precip_init(ncrms)
     ! Initialize precipitation related stuff
-
     use vars
     use micro_params
     use params
     use sat_mod
-
     implicit none
-    integer, intent(in) :: ncrms,icrm
+    integer, intent(in) :: ncrms
     real(crm_rknd) pratio, coef1, coef2,estw,esti,rrr1,rrr2
-    real*4 gammafff
-    external gammafff
-    integer k
+    real*4 :: gammafff
+    external :: gammafff
+    integer :: k,icrm
 
     gam3 = 3.
     gamr1 = 3.+b_rain
@@ -40,82 +37,56 @@ contains
     gamg1 = gammafff(gamg1)
     gamg2 = gammafff(gamg2)
     gamg3 = gammafff(gamg3)
-    !if(masterproc) then
-    ! print*,'gam3=',gam3
-    ! print*,'gamr1,gamr2,gamr3:',gamr1,gamr2,gamr3
-    ! print*,'gams1,gams2,gams3:',gams1,gams2,gams3
-    ! print*,'gamg1,gamg2,gamg3:',gamg1,gamg2,gamg3
-    !endif
+
     if(nint(gam3).ne.2) then
       if(masterproc)print*,'cannot compute gamma-function in precip_init. Exiting...'
       call task_abort
     end if
 
-    do k=1,nzm
+    do icrm = 1 , ncrms
+      do k=1,nzm
+        pratio = sqrt(1.29 / rho(k,icrm))
+        rrr1=393./(tabs0(k,icrm)+120.)*(tabs0(k,icrm)/273.)**1.5
+        rrr2=(tabs0(k,icrm)/273.)**1.94*(1000./pres(k,icrm))
+        estw = 100.*esatw_crm(tabs0(k,icrm))
+        esti = 100.*esati_crm(tabs0(k,icrm))
 
-      ! pratio = (1000. / pres(k,icrm)) ** 0.4
-      pratio = sqrt(1.29 / rho(k,icrm))
+        ! accretion by snow:
+        coef1 = 0.25 * pi * nzeros * a_snow * gams1 * pratio/(pi * rhos * nzeros/rho(k,icrm) ) ** ((3+b_snow)/4.)
+        coef2 = exp(0.025*(tabs0(k,icrm) - 273.15))
+        accrsi(k,icrm) =  coef1 * coef2 * esicoef
+        accrsc(k,icrm) =  coef1 * esccoef
+        coefice(k,icrm) =  coef2
 
-      rrr1=393./(tabs0(k,icrm)+120.)*(tabs0(k,icrm)/273.)**1.5
-      rrr2=(tabs0(k,icrm)/273.)**1.94*(1000./pres(k,icrm))
+        ! evaporation of snow:
+        coef1  =(lsub/(tabs0(k,icrm)*rv)-1.)*lsub/(therco*rrr1*tabs0(k,icrm))
+        coef2  = rv*tabs0(k,icrm)/(diffelq*rrr2*esti)
+        evaps1(k,icrm)  =  0.65*4.*nzeros/sqrt(pi*rhos*nzeros)/(coef1+coef2)/sqrt(rho(k,icrm))
+        evaps2(k,icrm)  =  0.49*4.*nzeros*gams2*sqrt(a_snow/(muelq*rrr1))/(pi*rhos*nzeros)**((5+b_snow)/8.) / (coef1+coef2) * rho(k,icrm)**((1+b_snow)/8.)*sqrt(pratio)
 
-      estw = 100.*esatw_crm(tabs0(k,icrm))
-      esti = 100.*esati_crm(tabs0(k,icrm))
+        ! accretion by graupel:
+        coef1 = 0.25*pi*nzerog*a_grau*gamg1*pratio/(pi*rhog*nzerog/rho(k,icrm))**((3+b_grau)/4.)
+        coef2 = exp(0.025*(tabs0(k,icrm) - 273.15))
+        accrgi(k,icrm) =  coef1 * coef2 * egicoef
+        accrgc(k,icrm) =  coef1 * egccoef
 
-      ! accretion by snow:
+        ! evaporation of graupel:
+        coef1  =(lsub/(tabs0(k,icrm)*rv)-1.)*lsub/(therco*rrr1*tabs0(k,icrm))
+        coef2  = rv*tabs0(k,icrm)/(diffelq*rrr2*esti)
+        evapg1(k,icrm)  = 0.65*4.*nzerog/sqrt(pi*rhog*nzerog)/(coef1+coef2)/sqrt(rho(k,icrm))
+        evapg2(k,icrm)  = 0.49*4.*nzerog*gamg2*sqrt(a_grau/(muelq*rrr1))/(pi * rhog * nzerog)**((5+b_grau)/8.) / (coef1+coef2) * rho(k,icrm)**((1+b_grau)/8.)*sqrt(pratio)
 
-      coef1 = 0.25 * pi * nzeros * a_snow * gams1 * pratio/ &
-      (pi * rhos * nzeros/rho(k,icrm) ) ** ((3+b_snow)/4.)
-      coef2 = exp(0.025*(tabs0(k,icrm) - 273.15))
-      accrsi(k,icrm) =  coef1 * coef2 * esicoef
-      accrsc(k,icrm) =  coef1 * esccoef
-      coefice(k,icrm) =  coef2
+        ! accretion by rain:
+        accrrc(k,icrm)=  0.25 * pi * nzeror * a_rain * gamr1 * pratio/(pi * rhor * nzeror / rho(k,icrm)) ** ((3+b_rain)/4.)* erccoef
 
-      ! evaporation of snow:
-
-      coef1  =(lsub/(tabs0(k,icrm)*rv)-1.)*lsub/(therco*rrr1*tabs0(k,icrm))
-      coef2  = rv*tabs0(k,icrm)/(diffelq*rrr2*esti)
-      evaps1(k,icrm)  =  0.65*4.*nzeros/sqrt(pi*rhos*nzeros)/(coef1+coef2)/sqrt(rho(k,icrm))
-      evaps2(k,icrm)  =  0.49*4.*nzeros*gams2*sqrt(a_snow/(muelq*rrr1))/ &
-      (pi*rhos*nzeros)**((5+b_snow)/8.) / (coef1+coef2) &
-      * rho(k,icrm)**((1+b_snow)/8.)*sqrt(pratio)
-
-      ! accretion by graupel:
-
-      coef1 = 0.25*pi*nzerog*a_grau*gamg1*pratio/&
-      (pi*rhog*nzerog/rho(k,icrm))**((3+b_grau)/4.)
-      coef2 = exp(0.025*(tabs0(k,icrm) - 273.15))
-      accrgi(k,icrm) =  coef1 * coef2 * egicoef
-      accrgc(k,icrm) =  coef1 * egccoef
-
-      ! evaporation of graupel:
-
-      coef1  =(lsub/(tabs0(k,icrm)*rv)-1.)*lsub/(therco*rrr1*tabs0(k,icrm))
-      coef2  = rv*tabs0(k,icrm)/(diffelq*rrr2*esti)
-      evapg1(k,icrm)  = 0.65*4.*nzerog/sqrt(pi*rhog*nzerog)/(coef1+coef2)/sqrt(rho(k,icrm))
-      evapg2(k,icrm)  = 0.49*4.*nzerog*gamg2*sqrt(a_grau/(muelq*rrr1))/ &
-      (pi * rhog * nzerog)**((5+b_grau)/8.) / (coef1+coef2) &
-      * rho(k,icrm)**((1+b_grau)/8.)*sqrt(pratio)
-
-
-      ! accretion by rain:
-
-      accrrc(k,icrm)=  0.25 * pi * nzeror * a_rain * gamr1 * pratio/ &
-      (pi * rhor * nzeror / rho(k,icrm)) ** ((3+b_rain)/4.)* erccoef
-
-      ! evaporation of rain:
-
-      coef1  =(lcond/(tabs0(k,icrm)*rv)-1.)*lcond/(therco*rrr1*tabs0(k,icrm))
-      coef2  = rv*tabs0(k,icrm)/(diffelq * rrr2 * estw)
-      evapr1(k,icrm)  =  0.78 * 2. * pi * nzeror / &
-      sqrt(pi * rhor * nzeror) / (coef1+coef2) / sqrt(rho(k,icrm))
-      evapr2(k,icrm)  =  0.31 * 2. * pi  * nzeror * gamr2 * &
-      0.89 * sqrt(a_rain/(muelq*rrr1))/ &
-      (pi * rhor * nzeror)**((5+b_rain)/8.) / (coef1+coef2) &
-      * rho(k,icrm)**((1+b_rain)/8.)*sqrt(pratio)
-
+        ! evaporation of rain:
+        coef1  =(lcond/(tabs0(k,icrm)*rv)-1.)*lcond/(therco*rrr1*tabs0(k,icrm))
+        coef2  = rv*tabs0(k,icrm)/(diffelq * rrr2 * estw)
+        evapr1(k,icrm)  =  0.78 * 2. * pi * nzeror / &
+        sqrt(pi * rhor * nzeror) / (coef1+coef2) / sqrt(rho(k,icrm))
+        evapr2(k,icrm)  =  0.31 * 2. * pi  * nzeror * gamr2 * 0.89 * sqrt(a_rain/(muelq*rrr1))/(pi * rhor * nzeror)**((5+b_rain)/8.) / (coef1+coef2) * rho(k,icrm)**((1+b_rain)/8.)*sqrt(pratio)
+      end do
     end do
-
 
   end subroutine precip_init
 
