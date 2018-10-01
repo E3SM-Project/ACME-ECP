cbb3c92d8f6dda4cb8212decc096100e30852d8c
Matt Norman <normanmr@ornl.gov>
2018-09-30 20:38:19 -0400

    pushed loop into advect_scalar.F90

--- components/cam/src/physics/crm/advect_all_scalars.F90
+++ components/cam/src/physics/crm/advect_all_scalars.F90
@@ -22,12 +22,10 @@ contains
     real(crm_rknd) :: micro_field_tmp(dimx1_s:dimx2_s, dimy1_s:dimy2_s, nzm, ncrms)
     real(crm_rknd) :: adv_tmp(nz,ncrms)
     real(crm_rknd) :: wle_tmp(nz,ncrms)
-    real(crm_rknd) :: esmt_offset    ! whannah - offset for advecting scalar momentum tracers
-
-    do icrm = 1 , ncrms
+    real(crm_rknd) :: esmt_offset(ncrms)    ! whannah - offset for advecting scalar momentum tracers
 
     !      advection of scalars :
-    call advect_scalar(ncrms,icrm,t(:,:,:,icrm),tadv(:,icrm),twle(:,icrm))
+    call advect_scalar(ncrms,t,tadv,twle)
 
     !    Advection of microphysics prognostics:
     do k = 1,nmicro_fields
@@ -39,28 +37,36 @@ contains
       .or. docloud.and.flag_precip(k).ne.1    & ! transport non-precipitation vars
 #endif
       .or. doprecip.and.flag_precip(k).eq.1 ) then
-        do kk = 1 , nzm
-          do j = dimy1_s,dimy2_s
-            do i = dimx1_s,dimx2_s
-              micro_field_tmp(i,j,kk,icrm) = micro_field(i,j,kk,k,icrm)
+        do icrm = 1 , ncrms
+          do kk = 1 , nzm
+            do j = dimy1_s,dimy2_s
+              do i = dimx1_s,dimx2_s
+                micro_field_tmp(i,j,kk,icrm) = micro_field(i,j,kk,k,icrm)
+              enddo
             enddo
           enddo
         enddo
-        do kk = 1 , nz
-          adv_tmp(kk,icrm) = mkadv(kk,k,icrm)
-          wle_tmp(kk,icrm) = mkwle(kk,k,icrm)
+        do icrm = 1 , ncrms
+          do kk = 1 , nz
+            adv_tmp(kk,icrm) = mkadv(kk,k,icrm)
+            wle_tmp(kk,icrm) = mkwle(kk,k,icrm)
+          enddo
         enddo
-        call advect_scalar(ncrms,icrm,micro_field_tmp(:,:,:,icrm),adv_tmp(:,icrm),wle_tmp(:,icrm))
-        do kk = 1 , nzm
-          do j = dimy1_s,dimy2_s
-            do i = dimx1_s,dimx2_s
-              micro_field(i,j,kk,k,icrm) = micro_field_tmp(i,j,kk,icrm)
+        call advect_scalar(ncrms,micro_field_tmp,adv_tmp,wle_tmp)
+        do icrm = 1 , ncrms
+          do kk = 1 , nzm
+            do j = dimy1_s,dimy2_s
+              do i = dimx1_s,dimx2_s
+                micro_field(i,j,kk,k,icrm) = micro_field_tmp(i,j,kk,icrm)
+              enddo
             enddo
           enddo
         enddo
-        do kk = 1 , nz
-          mkadv(kk,k,icrm) = adv_tmp(kk,icrm)
-          mkwle(kk,k,icrm) = wle_tmp(kk,icrm)
+        do icrm = 1 , ncrms
+          do kk = 1 , nz
+            mkadv(kk,k,icrm) = adv_tmp(kk,icrm)
+            mkwle(kk,k,icrm) = wle_tmp(kk,icrm)
+          enddo
         enddo
       endif
     end do
@@ -68,27 +74,33 @@ contains
     !    Advection of sgs prognostics:
     if(dosgs.and.advect_sgs) then
       do k = 1,nsgs_fields
-        do kk = 1 , nz
-          adv_tmp(kk,icrm) = sgsadv(kk,k,icrm)
-          wle_tmp(kk,icrm) = sgswle(kk,k,icrm)
+        do icrm = 1 , ncrms
+          do kk = 1 , nz
+            adv_tmp(kk,icrm) = sgsadv(kk,k,icrm)
+            wle_tmp(kk,icrm) = sgswle(kk,k,icrm)
+          enddo
         enddo
-        call advect_scalar(ncrms,icrm,sgs_field(:,:,:,icrm,k),adv_tmp(:,icrm),wle_tmp(:,icrm))
-        do kk = 1 , nz
-          sgsadv(kk,k,icrm) = adv_tmp(kk,icrm)
-          sgswle(kk,k,icrm) = wle_tmp(kk,icrm)
+        call advect_scalar(ncrms,sgs_field(:,:,:,:,k),adv_tmp,wle_tmp)
+        do icrm = 1 , ncrms
+          do kk = 1 , nz
+            sgsadv(kk,k,icrm) = adv_tmp(kk,icrm)
+            sgswle(kk,k,icrm) = wle_tmp(kk,icrm)
+          enddo
         enddo
       end do
     end if
 
     !   Precipitation fallout:
     if(doprecip) then
-      total_water_prec(icrm) = total_water_prec(icrm) + total_water(ncrms,icrm)
-      call micro_precip_fall(ncrms,icrm)
-      total_water_prec(icrm) = total_water_prec(icrm) - total_water(ncrms,icrm)
+      do icrm = 1 , ncrms
+        total_water_prec(icrm) = total_water_prec(icrm) + total_water(ncrms,icrm)
+        call micro_precip_fall(ncrms,icrm)
+        total_water_prec(icrm) = total_water_prec(icrm) - total_water(ncrms,icrm)
+      enddo
     end if
 
     ! advection of tracers:
-    !There aren't any of these
+    !There aren't any of these. We need to delete crmtracers.F90 too at some point
     !if(dotracers) then
     !  do k = 1,ntracers
     !    call advect_scalar(ncrms,icrm,tracer(:,:,:,k,icrm),tradv(:,k,icrm),trwle(:,k,icrm))
@@ -98,16 +110,19 @@ contains
 #if defined(SP_ESMT)
     ! whannah - the esmt_offset simply ensures that the scalar momentum
     ! tracers are positive definite during the advection calculation
-    esmt_offset = abs( minval( (/ minval(u_esmt(:,:,:,icrm)), minval(v_esmt(:,:,:,icrm)) /) ) ) + 50.
-    u_esmt(:,:,:,icrm) = u_esmt(:,:,:,icrm) + esmt_offset
-    v_esmt(:,:,:,icrm) = v_esmt(:,:,:,icrm) + esmt_offset
-    ! advection of scalar momentum tracers
-    call advect_scalar(ncrms,icrm,u_esmt(:,:,:,icrm),u_esmt_adv(:,icrm),u_esmt_wle(:,icrm))
-    call advect_scalar(ncrms,icrm,v_esmt(:,:,:,icrm),v_esmt_adv(:,icrm),v_esmt_wle(:,icrm))
-    u_esmt(:,:,:,icrm) = u_esmt(:,:,:,icrm) - esmt_offset
-    v_esmt(:,:,:,icrm) = v_esmt(:,:,:,icrm) - esmt_offset
-#endif
+    do icrm = 1 , ncrms
+      esmt_offset(icrm) = abs( minval( (/ minval(u_esmt(:,:,:,icrm)), minval(v_esmt(:,:,:,icrm)) /) ) ) + 50.
+      u_esmt(:,:,:,icrm) = u_esmt(:,:,:,icrm) + esmt_offset(icrm)
+      v_esmt(:,:,:,icrm) = v_esmt(:,:,:,icrm) + esmt_offset(icrm)
     enddo
+    ! advection of scalar momentum tracers
+    call advect_scalar(ncrms,u_esmt,u_esmt_adv,u_esmt_wle)
+    call advect_scalar(ncrms,v_esmt,v_esmt_adv,v_esmt_wle)
+    do icrm = 1 , ncrms
+      u_esmt(:,:,:,icrm) = u_esmt(:,:,:,icrm) - esmt_offset(icrm)
+      v_esmt(:,:,:,icrm) = v_esmt(:,:,:,icrm) - esmt_offset(icrm)
+    enddo
+#endif
 
   end subroutine advect_all_scalars
 
