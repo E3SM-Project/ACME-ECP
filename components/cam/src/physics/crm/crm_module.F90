9a47d8065c1db0916ef6c486f641b3c7c1145ab2
Matt Norman <normanmr@ornl.gov>
2018-12-14 17:21:12 -0500

    uvw on the GPU

--- components/cam/src/physics/crm/crm_module.F90
+++ components/cam/src/physics/crm/crm_module.F90
@@ -878,11 +878,6 @@ subroutine crm(lchnk, icol, ncrms, phys_stage, dt_gl, plev, &
       !      advection of scalars :
       call advect_all_scalars(ncrms)
 
-      !$acc exit data copyout(dudt,dvdt,dwdt,misc,adz,bet,tabs0,qv,qv0,qcl,qci,qn0,qpl,qpi,qp0,tabs,t,micro_field,ttend,qtend,utend,vtend,u,u0,v,v0,w,t0,dz,precsfc,precssfc,rho,qifall,tlatqi, &
-      !$acc&                  sstxy,sgs_field,sgs_field_diag,uhl,vhl,taux0,tauy0,z,z0,fluxbu,fluxbv,bflx,adzw,presi,tkelediss,tkesbdiss,tkesbshear,tkesbbuoy,grdf_x,grdf_y,grdf_z,tke2,tk2,tk,tke,tkh, &
-      !$acc&                  rhow,uwle,vwle,uwsb,vwsb) async(1)
-      !$acc wait(1)
-
       !-----------------------------------------------------------
       !    Convert velocity back from nondimensional form:
       call uvw(ncrms)
@@ -890,6 +885,10 @@ subroutine crm(lchnk, icol, ncrms, phys_stage, dt_gl, plev, &
       !----------------------------------------------------------
       !     Update boundaries for scalars to prepare for SGS effects:
       call boundaries(ncrms,3)
+
+      !$acc exit data copyout(dudt,dvdt,dwdt,misc,adz,bet,tabs0,qv,qv0,qcl,qci,qn0,qpl,qpi,qp0,tabs,t,micro_field,ttend,qtend,utend,vtend,u,u0,v,v0,w,t0,dz,precsfc,precssfc,rho,qifall,tlatqi, &
+      !$acc&                  sstxy,sgs_field,sgs_field_diag,uhl,vhl,taux0,tauy0,z,z0,fluxbu,fluxbv,bflx,adzw,presi,tkelediss,tkesbdiss,tkesbshear,tkesbbuoy,grdf_x,grdf_y,grdf_z,tke2,tk2,tk,tke,tkh, &
+      !$acc&                  rhow,uwle,vwle,uwsb,vwsb) async(1)
       !$acc wait(1)
 
       !---------------------------------------------------------
