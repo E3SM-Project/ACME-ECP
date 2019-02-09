8315b84277302fbd6b2f06f38349afa1be2b93ce
Matt Norman <normanmr@ornl.gov>
2019-02-08 20:01:06 -0500

    fcory,vg0,fcorzy,ug0

--- components/cam/src/physics/crm/coriolis.F90
+++ components/cam/src/physics/crm/coriolis.F90
@@ -25,9 +25,9 @@ contains
               ic=i+1
               v_av=0.25*(v(icrm,i,j,k)+v(icrm,i,jc,k)+v(icrm,ib,j,k)+v(icrm,ib,jc,k))
               w_av=0.25*(w(icrm,i,j,kc)+w(icrm,ib,j,kc)+w(icrm,i,j,k)+w(icrm,ib,j,k))
-              dudt(icrm,i,j,k,na)=dudt(icrm,i,j,k,na)+fcory(j,icrm)*(v_av-vg0(k,icrm))-fcorzy(j,icrm)*w_av
+              dudt(icrm,i,j,k,na)=dudt(icrm,i,j,k,na)+fcory(icrm,j)*(v_av-vg0(icrm,k))-fcorzy(icrm,j)*w_av
               u_av=0.25*(u(icrm,i,j,k)+u(icrm,ic,j,k)+u(icrm,i,jb,k)+u(icrm,ic,jb,k))
-              dvdt(icrm,i,j,k,na)=dvdt(icrm,i,j,k,na)-0.5*(fcory(j,icrm)+fcory(jb,icrm))*(u_av-ug0(k,icrm))
+              dvdt(icrm,i,j,k,na)=dvdt(icrm,i,j,k,na)-0.5*(fcory(icrm,j)+fcory(icrm,jb))*(u_av-ug0(icrm,k))
             end do ! i
           end do ! j
         end do ! k
@@ -42,8 +42,8 @@ contains
               ib=i-1
               ic=i+1
               w_av=0.25*(w(icrm,i,j,kc)+w(icrm,ib,j,kc)+w(icrm,i,j,k)+w(icrm,ib,j,k))
-              dudt(icrm,i,j,k,na)=dudt(icrm,i,j,k,na)+fcory(j,icrm)*(v(icrm,i,j,k)-vg0(k,icrm))-fcorzy(j,icrm)*w_av
-              dvdt(icrm,i,j,k,na)=dvdt(icrm,i,j,k,na)-fcory(j,icrm)*(u(icrm,i,j,k)-ug0(k,icrm))
+              dudt(icrm,i,j,k,na)=dudt(icrm,i,j,k,na)+fcory(icrm,j)*(v(icrm,i,j,k)-vg0(icrm,k))-fcorzy(icrm,j)*w_av
+              dvdt(icrm,i,j,k,na)=dvdt(icrm,i,j,k,na)-fcory(icrm,j)*(u(icrm,i,j,k)-ug0(icrm,k))
             end do ! i
           end do ! i
         end do ! k
